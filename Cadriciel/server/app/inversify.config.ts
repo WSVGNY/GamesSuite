10a86206875641141168ec7e3e6d6aae66afaae6
charlesnollet <chrlsnllt@gmail.com>
2018-01-16 12:21:23 -0500

    [MC-13] Separated server-side into two folders

--- Cadriciel/server/app/inversify.config.ts
+++ Cadriciel/server/app/inversify.config.ts
@@ -4,13 +4,15 @@ import { Server } from "./server";
 import { Application } from "./app";
 import { Index } from "./routes/index";
 import { Routes } from "./routes";
+import { EmptyGrid } from "./mot-croise/emptyGrid";
+
 
 const container: Container = new Container();
 
 container.bind(Types.Server).to(Server);
 container.bind(Types.Application).to(Application);
 container.bind(Types.Routes).to(Routes);
-
 container.bind(Types.Index).to(Index);
+container.bind(Types.EmptyGrid).to(EmptyGrid);
 
 export { container };
--- Cadriciel/server/app/routes.ts
+++ Cadriciel/server/app/routes.ts
@@ -3,11 +3,13 @@ import { Router, Request, Response, NextFunction } from "express";
 
 import Types from "./types";
 import { Index } from "./routes/index";
+import { EmptyGrid } from "./mot-croise/emptyGrid";
 
 @injectable()
 export class Routes {
 
-    public constructor( @inject(Types.Index) private index: Index) { }
+    public constructor( @inject(Types.Index) private index: Index,
+                        @inject(Types.EmptyGrid) private emptyGrid: EmptyGrid ) { }
 
     public get routes(): Router {
         const router: Router = Router();
@@ -15,7 +17,7 @@ export class Routes {
         router.get("/",
             (req: Request, res: Response, next: NextFunction) => this.index.helloWorld(req, res, next));
         router.get("/emptyGridGet",
-            (req: Request, res: Response, next: NextFunction) => this.index.emptyGrid(req, res, next));
+            (req: Request, res: Response, next: NextFunction) => this.emptyGrid.emptyGrid(req, res, next));
 
         return router;
     }
