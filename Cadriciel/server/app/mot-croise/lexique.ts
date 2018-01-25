0100c2e4ffc0ce372aad85afcb30afdca1b831ce
silegc <simon.legault@polymtl.ca>
2018-01-24 21:02:14 -0500

    Started to apply TsLint to server

--- Cadriciel/server/app/mot-croise/lexique.ts
+++ Cadriciel/server/app/mot-croise/lexique.ts
@@ -8,13 +8,13 @@ module Route {
     export class Lexique {
 
         private readonly BASE_URL: string = "https://api.datamuse.com/words?";
-        mots: string;
+        private mots: string;
 
-        public getDefinition(req: Request, res: Response, next: NextFunction, mot:String): void {
-            https.get(this.BASE_URL + "sp="+mot+"&md=d", (ress) => {
+        public getDefinition(req: Request, res: Response, next: NextFunction, mot: String): void {
+            https.get(this.BASE_URL + "sp=" + mot + "&md=d", (ress) => {
                 ress.on('data', (d) => {
                     this.mots = JSON.parse(d.toString());
-                    res.send(this.mots[0]["defs"]+"\n");
+                    res.send(this.mots[0]["defs"] + "\n");
                 });
             }).on('error', (e) => {
                 console.error(e);
@@ -22,21 +22,22 @@ module Route {
         }
 
         public getListeMotSelonNbLettres(req: Request, res: Response, next: NextFunction, nbLettres: Number): void {
-            let URLOptions: string = "sp=";
-            for(let i = 0; i < nbLettres; i++){
+            let URLOptions = "sp=";
+            for(let i = 0; i < nbLettres; i++) {
                 URLOptions += "?";
             }
             this.getDeApi(req, res, next, URLOptions);
         }
-        
-        private getDeApi(req: Request, res: Response, next: NextFunction, URLOptions: string): Object[]{
+
+        private getDeApi(req: Request, res: Response, next: NextFunction, URLOptions: string): Object[] {
             https.get(this.BASE_URL + URLOptions, (ress) => {
-                ress.on('data', (d) => {
+                ress.on("data", (d) => {
                     res.send(JSON.parse(d.toString()));
                 });
-            }).on('error', (e) => {
+            }).on("error", (e) => {
                 console.error(e);
             });
+
             return null;
         }
     }
