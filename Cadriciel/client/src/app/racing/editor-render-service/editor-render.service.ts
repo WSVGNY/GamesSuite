250d0df77a6ab6282de46ca8dcd4caed81614b0f
wisev <wsvgny@gmail.com>
2018-01-30 18:55:16 -0500

    [CA-23] Created trackVertices class skeleton

--- Cadriciel/client/src/app/racing/editor-render-service/editor-render.service.ts
+++ Cadriciel/client/src/app/racing/editor-render-service/editor-render.service.ts
@@ -115,4 +115,3 @@ export class EditorRenderService {
     this.scene.add(this.line);
   }
 }
-
---
+++ Cadriciel/client/src/app/racing/trackVertices.ts
@@ -0,0 +1,12 @@
+// import { Geometry, MeshBasicMaterial, Object3D } from "three";
+
+export class TrackVertices {
+    public vertices: Array<boolean> = new Array();
+    public constructor() {
+
+    }
+
+    public addVertex(): void {
+
+    }
+}
