f911959f6a8a6655ac57ad53544435ccefa80216
wisev <wsvgny@gmail.com>
2018-01-30 20:45:19 -0500

    [CA-23] Implemented add ans remove vertex in trackvertices.ts

--- Cadriciel/client/src/app/racing/trackVertices.ts
+++ Cadriciel/client/src/app/racing/trackVertices.ts
@@ -1,12 +1,28 @@
-// import { Geometry, MeshBasicMaterial, Object3D } from "three";
+import { SphereGeometry, MeshBasicMaterial, Mesh } from "three";
+
+const RED: number = 0xFF1101;
+const RADIUS: number = 8;
+const VERTEX_GEOMETRY: SphereGeometry  = new SphereGeometry(RADIUS, RADIUS, RADIUS);
+const VERTEX_MATERIAL: MeshBasicMaterial = new MeshBasicMaterial ( {color : RED});
 
 export class TrackVertices {
-    public vertices: Array<boolean> = new Array();
-    public constructor() {
 
+    private vertices: Array<Mesh>;
+    private scene: THREE.Scene;
+
+    public constructor(scene: THREE.Scene) {
+        this.scene = scene;
+        this.vertices = new Array();
     }
 
-    public addVertex(): void {
+    public addVertex(x: number, y: number): void {
+        const vertex: Mesh = new Mesh(VERTEX_GEOMETRY, VERTEX_MATERIAL);
+        vertex.position.set(x, y, 0);
+        this.scene.add (vertex);
+        this.vertices.push(vertex);
+    }
 
+    public removeLastVertex(): void {
+        this.scene.remove(this.vertices.pop());
     }
 }
