c73169c6ee7e7bf61c218727210dafe43dc8fca6
silegc <simon.legault@polymtl.ca>
2018-02-04 15:47:38 -0500

    [CA-13] CameraFollow is tested and DONE

--- Cadriciel/client/src/app/racing/render-service/render.service.spec.ts
+++ Cadriciel/client/src/app/racing/render-service/render.service.spec.ts
@@ -1,32 +1,27 @@
 import { TestBed, inject } from "@angular/core/testing";
-
 import { RenderService } from "./render.service";
-import { ElementRef } from "@angular/core/src/linker/element_ref";
 import { Object3D } from "three";
-import { Car} from "./../car/car";
 
 describe("RenderService", () => {
-    let render: RenderService;
-    beforeEach(async (done: () => void) => {
+
+    beforeEach(() => {
 
         TestBed.configureTestingModule({
             providers: [RenderService]
         });
-
-
-        render = new RenderService;
-
-        //await render.initialize();
-        done();
     });
 
     it("should be created", inject([RenderService], (service: RenderService) => {
         expect(service).toBeTruthy();
     }));
 
-    it("camera should be a child of _car", inject([RenderService], (service: RenderService) => {
-
-        //const cam: Object3D = service.car.getChild("PLAYER_CAMERA");
-        //expect(cam).toBeDefined();
-    });
+    it("camera should be a child of _car", inject([RenderService], (done: () => void) => {
+        let render: RenderService;
+        render = new RenderService();
+        render.initialize(undefined).then(() => {
+            const cam: Object3D = render.car.getChild("PLAYER_CAMERA");
+            expect(cam).toBeDefined();
+            done();
+        }).catch(done);
+    }));
 });
