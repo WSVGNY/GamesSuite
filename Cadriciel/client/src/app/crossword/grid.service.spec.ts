c0a282a39285ff7a76c50492e35056f47b5bc297
silegc <simon.legault@polymtl.ca>
2018-02-04 16:27:26 -0500

    Reviewed code client side (grid, basicService)

--- Cadriciel/client/src/app/crossword/grid.service.spec.ts
+++ Cadriciel/client/src/app/crossword/grid.service.spec.ts
@@ -1,9 +1,6 @@
 import { TestBed, inject } from "@angular/core/testing";
-
 import { GridService } from "./grid.service";
-
 import { AppModule } from "../app.module";
-
 import { APP_BASE_HREF } from "@angular/common";
 
 describe("GridService", () => {
