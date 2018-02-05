d7b59d5aae407cd7196a4affe6c57dad88aaef06
wisev <wsvgny@gmail.com>
2018-02-05 16:03:41 -0500

    [CA-23] Fixed point of adding points to scene

--- Cadriciel/client/src/app/racing/trackVertices.spec.ts
+++ Cadriciel/client/src/app/racing/trackVertices.spec.ts
@@ -1,5 +1,5 @@
 import { TrackVertices, VERTEX_GEOMETRY, SIMPLE_VERTEX_MATERIAL, LINE_MATERIAL } from "./trackVertices";
-import { Scene, Vector3, Mesh, Geometry, Line } from "three";
+import { Scene, Vector3, Mesh, Geometry, Line, Object3D } from "three";
 
 describe("TrackVertices", () => {
     let scene: Scene;
@@ -12,7 +12,8 @@ describe("TrackVertices", () => {
 
     it("should cerate a point in the scene", ()  => {
         trackVertices.addVertex(new Vector3( 0, 0, 0 ));
-        expect(scene.getChildByName("vertex0") === null).toBeFalsy();
+        const vertex: Object3D = scene.getObjectByName("Start");
+        expect(vertex).toBeDefined();
     });
 
     it("should create a connection between two points", ()  => {
@@ -21,7 +22,7 @@ describe("TrackVertices", () => {
         vertex0.position.set(0, 0, 0 );
         vertex1.position.set(0, 0, 0 );
         trackVertices.addConnection(vertex0, vertex1);
-        expect(scene.getChildByName("connection0").name === null).toBeFalsy();
+        expect(scene.getChildByName("connection0")).toBeDefined();
     });
 
     it("first vertex of new connection should be the point before it", ()  => {
@@ -66,25 +67,11 @@ describe("TrackVertices", () => {
         expect(scene.).toBeFalsy();
     });
 */
-    /*it("should remove a point from the list of points and the connection assiciated this it", () => {
-        trackVerticies.removeLastVertex();
-        result = scene.getChildByName("vertex1").name === "vertex1" || scene.getChildByName("connection1").name === "connexion1" ;
-        expect(result).toBeFalsy();
-    });
-
-    it("should  a second and a third point plus the connextion between them", () => {
-        trackVerticies.addVertex(vector2);
-        trackVerticies.addVertex(vector3);
-        result = scene.getChildByName("vertex1").name === "vertex1"
-        && scene.getChildByName("vertex2").name === "vertex2"
-        && scene.getChildByName("connection1").name === "connexion1"
-        && scene.getChildByName("connection2").name === "connexion2";
-        expect(result).toBeTruthy();
-    });
-
+/*
     it("should  loop the track when we add a point on the start Point ", () => {
-        trackVerticies.addVertex(vector1);
+        trackVertices.addVertex(vector1);
         result = listOfPoints.$isComplete();
         expect(result).toBeTruthy();
-    });*/
+    });
+    */
 });
