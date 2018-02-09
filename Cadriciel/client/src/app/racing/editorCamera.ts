ef57db52cbb8cc80469a238edc8d59147eba90ad
wisev <wsvgny@gmail.com>
2018-02-09 01:34:18 -0500

    [CA-23] Grouped the rendering functions in the editorCamera class

--- Cadriciel/client/src/app/racing/editorCamera.ts
+++ Cadriciel/client/src/app/racing/editorCamera.ts
@@ -3,24 +3,25 @@ import { OrthographicCamera, Vector3 } from "three";
 const HALF: number = 0.5;
 const FAR_CLIPPING_PLANE: number = 1000;
 const NEAR_CLIPPING_PLANE: number = 1;
-const VIEW_SIZE: number = 1000;
 
 export class EditorCamera {
 
     private camera: OrthographicCamera;
     private aspectRatio: number = 0;
+    private viewSize: number = 0;
 
-    public constructor(aspectRatio: number) {
+    public constructor(aspectRatio: number, viewSize: number) {
         this.aspectRatio = aspectRatio;
+        this.viewSize = viewSize;
         this.initialise();
     }
 
     private initialise(): void {
         this.camera = new OrthographicCamera(
-            -this.aspectRatio * VIEW_SIZE * HALF,
-            this.aspectRatio * VIEW_SIZE * HALF,
-            VIEW_SIZE * HALF,
-            -VIEW_SIZE * HALF,
+            -this.aspectRatio * this.viewSize * HALF,
+            this.aspectRatio * this.viewSize * HALF,
+            this.viewSize * HALF,
+            -this.viewSize * HALF,
             NEAR_CLIPPING_PLANE,
             FAR_CLIPPING_PLANE);
     }
