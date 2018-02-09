a8fead824a6b6aef2c54d7a4d765e71a9c04cfbe
Philippe Kavalec <philippe_kavalec@hotmail.com>
2018-02-08 21:26:23 -0500

    [CA-23] Testing doing weird stuff

--- Cadriciel/client/src/app/racing/admin/admin.component.spec.ts
+++ Cadriciel/client/src/app/racing/admin/admin.component.spec.ts
@@ -26,21 +26,4 @@ describe("AdminComponent", () => {
     it("should create", () => {
         expect(component).toBeTruthy();
     });
-
-    it("should get tracks", async () => {
-        expect(component["tracks"].length).toBeGreaterThan(0);
-    });
-
-    it("should create a new track", async () => {
-        const oldLength: number = component["tracks"].length;
-        await component.newTrack("test track");
-        expect(component["tracks"].length).toEqual(oldLength + 1);
-    });
-
-    it("should delete tracks", async () => {
-        const id: number = component["tracks"].length - 1;
-        const oldLength: number = component["tracks"].length;
-        await component.deleteTrack(id);
-        expect(component["tracks"].length).toEqual(oldLength - 1);
-    });
 });
--- Cadriciel/client/src/app/racing/track-service/track.service.spec.ts
+++ Cadriciel/client/src/app/racing/track-service/track.service.spec.ts
@@ -2,8 +2,12 @@ import { TestBed, inject } from "@angular/core/testing";
 import { TrackService } from "./track.service";
 import { AppModule } from "../../app.module";
 import { APP_BASE_HREF } from "@angular/common";
+import { Track } from "../../../../../common/racing/track";
 
 describe("TrackService", () => {
+    let originalTimeout: number;
+    const TIMEOUT: number = 10000;
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [
@@ -12,9 +16,71 @@ describe("TrackService", () => {
             ],
             imports: [AppModule]
         });
+        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = TIMEOUT;
+    });
+
+    afterEach(() => {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
     });
 
     it("should be created", inject([TrackService], (service: TrackService) => {
         expect(service).toBeTruthy();
+    }));
+
+    it("should get track list", inject([TrackService], (service: TrackService, done: DoneFn) => {
+
+        service.getTrackList()
+            .subscribe((tracksFromServer: Track[]) => {
+                const tracks: Track[] = tracksFromServer;
+                expect(tracks.length).toBeGreaterThan(0);
+                done();
+            });
+    }));
+
+    it("should get track from id", inject([TrackService], (service: TrackService, done: DoneFn) => {
+        service.getTrackFromId(1)
+            .subscribe((trackFromServer: Track) => {
+                const track: Track = new Track(trackFromServer["id"], trackFromServer["name"]);
+                expect(track.$id).toEqual(1);
+                done();
+            });
+    }));
+
+    it("should create a new track", inject([TrackService], (service: TrackService, done: DoneFn) => {
+        service.getTrackList()
+            .subscribe((tracksFromServer: Track[]) => {
+                const tracks: Track[] = tracksFromServer;
+                service.newTrack("Test Track")
+                    .subscribe((newTrackFromServer: Track[]) => {
+                        expect(tracks.length).toBeLessThan(newTrackFromServer.length);
+                        done();
+                    });
+            });
+    }));
+
+    it("should edit a track ", inject([TrackService], (service: TrackService, done: DoneFn) => {
+        service.getTrackList()
+            .subscribe((tracksFromServer: Track[]) => {
+                const tracks: Track[] = tracksFromServer;
+                service.putTrack(new Track(tracks[tracks.length - 1]["id"], "New name"))
+                    .subscribe((editedTrack: Track) => {
+                        const track: Track = new Track(editedTrack["id"], editedTrack["name"]);
+                        expect(track.$name).toEqual("New name");
+                        done();
+                    });
+            });
+    }));
+
+    it("should delete tracks", inject([TrackService], (service: TrackService, done: DoneFn) => {
+        service.getTrackList()
+            .subscribe((tracksFromServer: Track[]) => {
+                const tracks: Track[] = tracksFromServer;
+                service.deleteTrack(tracks.length)
+                    .subscribe((newTrackFromServer: Track[]) => {
+                        expect(tracks.length).toBeGreaterThan(newTrackFromServer.length);
+                        done();
+                    });
+            });
     }));
 });
