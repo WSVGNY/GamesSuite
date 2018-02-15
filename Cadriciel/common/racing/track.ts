af8c07c153aa570eb10e3f4fbe2fa017b3ca80ef
wisev <wsvgny@gmail.com>
2018-02-15 17:33:42 -0500

    [CA-01] Added vertices attribute in track.ts

--- Cadriciel/common/racing/track.ts
+++ Cadriciel/common/racing/track.ts
@@ -3,13 +3,16 @@ export interface ITrack {
     _id: string;
     track: {
         _name: string;
+        vertices: Array<Vector3>;
     };
 }
 export class Track {
     private _name: string;
+    private _vertices: Array<Vector3>;
 
     public constructor(rawTrack: ITrack) {
         this._name = rawTrack.track._name;
+        this._vertices = rawTrack.track.vertices;
     };
 
     public get name(): string {
@@ -18,6 +21,14 @@ export class Track {
 
     public set name(value: string) {
         this._name = value;
+    }
+
+    public get vertices(): Array<Vector3> {
+        return this._vertices;
+    }
+
+    public set vertices(value: Array<Vector3>) {
+        this._vertices = value;
     }
 
     // public static castStringToTrack(rawString: string): Track {
