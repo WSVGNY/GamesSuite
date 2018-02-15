4f3c2ebde0333c8bf8421bc70c8e8f45b5359b82
silegc <simon.legault@polymtl.ca>
2018-02-15 14:03:53 -0500

    [CA-09] Created template for command pattern

--- Cadriciel/client/src/app/racing/AI/car-ai.service.ts
+++ Cadriciel/client/src/app/racing/AI/car-ai.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from "@angular/core";
-//import { Car } from "../car/car";
+import { Car } from "../car/car";
 import { CommandController } from "../commandController";
+import { GoFoward } from "../commands/carAICommands/goFoward";
 
 @Injectable()
 export class CarAiService {
   private _aiControl: CommandController;
 
-  public constructor(/*private _car: Car*/) {
-    //_car = new Car();
+  public constructor(private _car: Car) {
+    _car = new Car();
     this._aiControl = new CommandController();
   }
 
   public calculateNewPosition(): void {
-    //this._aiControl.setCommand()
+    this._aiControl.setCommand(new GoFoward(this._car));
   }
 }
