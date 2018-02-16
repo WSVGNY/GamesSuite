b7a4dd35a7b940056abb969376b421aeda8dbf83
wisev <wsvgny@gmail.com>
2018-02-16 10:21:12 -0500

    [CA-09] Added command added command so the car turns in circle

--- Cadriciel/client/src/app/racing/AI/car-ai.service.ts
+++ Cadriciel/client/src/app/racing/AI/car-ai.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from "@angular/core";
 import { Car } from "../car/car";
 import { CommandController } from "../commandController";
 import { GoFoward } from "../commands/carAICommands/goFoward";
+import { TurnLeft } from "../commands/carAICommands/turnLeft";
 import { Track } from "../../../../../common/racing/track";
 
 @Injectable()
 export class CarAiService {
     private _aiControl: CommandController;
     private _isGoingForward: boolean = false;
+    private _isSteeringLeft: boolean = false;
 
     public constructor(private _car: Car, private _track: Track) {
         this._aiControl = new CommandController();
@@ -22,9 +24,17 @@ export class CarAiService {
         }
     }
 
+    private goLeft(): void {
+        this._aiControl.setCommand(new TurnLeft(this._car));
+        this._aiControl.execute();
+    }
+
     public update(): void {
         if (!this._isGoingForward) {
             this.goForward();
+        }
+        if (!this._isSteeringLeft) {
+            this.goLeft();
         }
     }
 }
