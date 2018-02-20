cefa05f408ca340c76b64ae07111ba7fd6f0bdb6
chbinc <chelsy.binet@polymtl.ca>
2018-02-20 10:32:05 -0500

    modification nom du fichier ai

--- Cadriciel/client/src/app/app.component.ts
+++ Cadriciel/client/src/app/app.component.ts
@@ -8,7 +8,6 @@ import { Component } from "@angular/core";
 export class AppComponent {
 
     public readonly title: string = "LOG2990";
-
     public showImages(): void {
         document.getElementById("image1").style.display = "inline-block";
         document.getElementById("image2").style.display = "inline-block";
