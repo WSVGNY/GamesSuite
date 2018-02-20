a34cb5a954dd79f78c5c07cf6b4e09f8a5e0b32b
chbinc <chelsy.binet@polymtl.ca>
2018-02-20 18:59:05 -0500

    [MC-04]on peut maintenant cliquer sur la grille

--- Cadriciel/client/src/app/crossword/crossword.component.ts
+++ Cadriciel/client/src/app/crossword/crossword.component.ts
@@ -32,7 +32,7 @@ export class CrosswordComponent {
             this.boxes = this.grid.boxes;
             this.words = this.grid.words;
             this.showLoader = false;
-            document.getElementById("gridHider").style.background = "rgba(255, 255, 255, 0)";
+            document.getElementById("gridHider").style.visibility = "hidden";
         });
     }
 
@@ -47,7 +47,7 @@ export class CrosswordComponent {
     public makeEasyGrid(): void {
         this.difficulty = Difficulty.Easy;
         this.showLoader = true;
-        document.getElementById("gridHider").style.background = "white";
+        document.getElementById("gridHider").style.visibility = "visible";
         this.createGrid();
     }
 
@@ -55,14 +55,14 @@ export class CrosswordComponent {
         this.difficulty = Difficulty.Medium;
         this.createGrid();
         this.showLoader = true;
-        document.getElementById("gridHider").style.background = "white";
+        document.getElementById("gridHider").style.visibility = "visible";
     }
 
     public makeHardGrid(): void {
         this.difficulty = Difficulty.Hard;
         this.createGrid();
         this.showLoader = true;
-        document.getElementById("gridHider").style.background = "white";
+        document.getElementById("gridHider").style.visibility = "visible";
     }
 
     public hide(): void {
