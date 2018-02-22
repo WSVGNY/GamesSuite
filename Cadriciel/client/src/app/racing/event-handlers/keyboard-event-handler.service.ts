68b28a567c39b2cfcde0c1258731008be7cc4dd1
wisev <wsvgny@gmail.com>
2018-02-21 23:16:20 -0500

    [CA-08] Completed the handleKeyDown and handleKeyUp functions

--- Cadriciel/client/src/app/racing/event-handlers/keyboard-event-handler.service.ts
+++ Cadriciel/client/src/app/racing/event-handlers/keyboard-event-handler.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from "@angular/core";
+import { Car } from "../car/car";
 import { CommandController } from "../commandController";
+import { GoFoward } from "../commands/carAICommands/goFoward";
+import { TurnLeft } from "../commands/carAICommands/turnLeft";
+import { TurnRight} from "../commands/carAICommands/turnRight";
+import { Brake } from "../commands/carAICommands/brake";
+import { ReleaseAccelerator } from "../commands/carAICommands/releaseAccelerator";
+import { ReleaseSteering } from "../commands/carAICommands/releaseSteering";
 
 const ACCELERATE_KEYCODE: number = 87;  // w
 const LEFT_KEYCODE: number = 65;        // a
@@ -9,40 +16,51 @@ const RIGHT_KEYCODE: number = 68;       // d
 @Injectable()
 export class KeyboardEventHandlerService {
 
-  private _editorControl: CommandController;
+  private _carControl: CommandController;
 
   public constructor() {}
 
-  public handleKeyDown(event: KeyboardEvent): void {
+  public initialize(): void {
+    this._carControl = new CommandController();
+  }
+
+  public handleKeyDown(event: KeyboardEvent, car: Car): void {
     switch (event.keyCode) {
         case ACCELERATE_KEYCODE:
-            this._playerCar.accelerate();
+            this._carControl.setCommand(new GoFoward(car));
+            this._carControl.execute();
             break;
         case LEFT_KEYCODE:
-            this._playerCar.steerLeft();
+            this._carControl.setCommand(new TurnLeft(car));
+            this._carControl.execute();
             break;
         case RIGHT_KEYCODE:
-            this._playerCar.steerRight();
+            this._carControl.setCommand(new TurnRight(car));
+            this._carControl.execute();
             break;
         case BRAKE_KEYCODE:
-            this._playerCar.reverse();
+            this._carControl.setCommand(new Brake(car));
+            this._carControl.execute();
             break;
         default:
             break;
     }
   }
 
-  public handleKeyUp(event: KeyboardEvent): void {
+  public handleKeyUp(event: KeyboardEvent, car: Car): void {
     switch (event.keyCode) {
         case ACCELERATE_KEYCODE:
-            this._playerCar.releaseAccelerator();
+            this._carControl.setCommand(new ReleaseAccelerator(car));
+            this._carControl.execute();
             break;
         case LEFT_KEYCODE:
         case RIGHT_KEYCODE:
-            this._playerCar.releaseSteering();
+            this._carControl.setCommand(new ReleaseSteering(car));
+            this._carControl.execute();
             break;
         case BRAKE_KEYCODE:
-            this._playerCar.releaseReverse();
+            this._carControl.setCommand(new Brake(car));
+            this._carControl.execute();
             break;
         default:
             break;
