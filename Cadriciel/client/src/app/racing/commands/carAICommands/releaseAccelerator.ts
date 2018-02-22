e80196437f4e1b641dd7d9fa1c72bc5f297d74f8
wsvgny@gmail.com <wsvgny@gmail.com>
2018-02-21 23:05:23 -0500

    [CA-08] Added releaseAccelerator command

---
+++ Cadriciel/client/src/app/racing/commands/carAICommands/releaseAccelerator.ts
@@ -0,0 +1,13 @@
+import { AbstractCarAICommand } from "./../abstractCarAICommand";
+import { Car } from "../../car/car";
+
+export class ReleaseSteering extends AbstractCarAICommand {
+
+    public constructor(car: Car) {
+        super(car);
+    }
+
+    public execute(): void {
+        this._car.releaseAccelerator();
+    }
+}
