80dea946785fb8f79a590462c6a51fad44c9c7a3
silegc <simon.legault@polymtl.ca>
2018-02-24 17:09:25 -0500

    Created an axis object

--- Cadriciel/client/src/app/racing/artificial-intelligence/ai-debug.ts
+++ Cadriciel/client/src/app/racing/artificial-intelligence/ai-debug.ts
@@ -5,12 +5,13 @@ import { Car } from "../car/car";
 
 export class AIDebug {
     private readonly AXIS_LENGTH: number = 5;
+    private readonly X_AXIS: number = 0;
+    private readonly Y_AXIS: number = 1;
+    private readonly Z_AXIS: number = 2;
 
     private _debugGroup: Group;
     private _carHelper: BoxHelper;
-    private _axisX: VectorHelper;
-    private _axisY: VectorHelper;
-    private _axisZ: VectorHelper;
+    private _axis: VectorHelper[] = [];
     private _carVectorHelper: VectorHelper;
     private _distanceVectorHelper: VectorHelper;
     private _turningVectorHelper: VectorHelper;
@@ -20,9 +21,9 @@ export class AIDebug {
     }
     private initializeDebugMode(): void {
         this._debugGroup = new Group;
-        this._axisX = new VectorHelper(RED);
-        this._axisY = new VectorHelper(GREEN);
-        this._axisZ = new VectorHelper(BLUE);
+        this._axis.push(new VectorHelper(RED));
+        this._axis.push(new VectorHelper(GREEN));
+        this._axis.push(new VectorHelper(BLUE));
 
         this._carVectorHelper = new VectorHelper(PINK);
         this._distanceVectorHelper = new VectorHelper(WHITE);
@@ -30,18 +31,18 @@ export class AIDebug {
         this._carHelper = new BoxHelper(this._car);
 
         this._debugGroup.add(this._carHelper);
-        this._debugGroup.add(this._axisX);
-        this._debugGroup.add(this._axisY);
-        this._debugGroup.add(this._axisZ);
+        for (const axis of this._axis) {
+            this._debugGroup.add(axis);
+        }
         this._debugGroup.add(this._carVectorHelper);
         this._debugGroup.add(this._distanceVectorHelper);
         this._debugGroup.add(this._turningVectorHelper);
     }
 
     public updateDebugMode(carPosition: Vector3, projection: Vector3, pointOnLine: Vector3, turningPoint: Vector3, vertice: Vector3): void {
-        this._axisX.update(carPosition, carPosition.clone().add(new Vector3(this.AXIS_LENGTH, 0, 0)));
-        this._axisY.update(carPosition, carPosition.clone().add(new Vector3(0, this.AXIS_LENGTH, 0)));
-        this._axisZ.update(carPosition, carPosition.clone().add(new Vector3(0, 0, this.AXIS_LENGTH)));
+        this._axis[this.X_AXIS].update(carPosition, carPosition.clone().add(new Vector3(this.AXIS_LENGTH, 0, 0)));
+        this._axis[this.Y_AXIS].update(carPosition, carPosition.clone().add(new Vector3(0, this.AXIS_LENGTH, 0)));
+        this._axis[this.Z_AXIS].update(carPosition, carPosition.clone().add(new Vector3(0, 0, this.AXIS_LENGTH)));
         this._carHelper.update(this._car);
         this._carVectorHelper.update(carPosition, projection);
         this._distanceVectorHelper.update(projection, pointOnLine);
