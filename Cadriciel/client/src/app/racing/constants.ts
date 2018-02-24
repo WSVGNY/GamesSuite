396d96d8f68fe8fef91bb9a136319c3dcfc22e66
silegc <simon.legault@polymtl.ca>
2018-02-24 16:42:42 -0500

    Green wasnt green

--- Cadriciel/client/src/app/racing/constants.ts
+++ Cadriciel/client/src/app/racing/constants.ts
@@ -20,7 +20,7 @@ export const WHITE: number = 0xFFFFFF;
 export const PINK: number = 0xFF00BF;
 export const BLUE: number = 0x0066FF;
 export const RED: number = 0xFF0000;
-export const GREEN: number = 0x0000FF;
+export const GREEN: number = 0x00FF00;
 export const ORANGE: number = 0xFF6600;
 
 // Texture Paths
