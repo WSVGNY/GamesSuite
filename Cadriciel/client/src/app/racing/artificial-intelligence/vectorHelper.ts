e83ca673e23b35494c7b41e22724dade3d3c08da
Philippe Kavalec <philippe_kavalec@hotmail.com>
2018-02-24 16:33:13 -0500

    Fixed debug mode

--- Cadriciel/client/src/app/racing/artificial-intelligence/vectorHelper.ts
+++ Cadriciel/client/src/app/racing/artificial-intelligence/vectorHelper.ts
@@ -1,19 +1,15 @@
 import { Vector3, LineBasicMaterial, Geometry, Line } from "three";
 
-export class VectorHelper {
-
-    public visual: Line;
-    private material: LineBasicMaterial;
+export class VectorHelper extends Line {
 
     public constructor(lineColor: number) {
-        this.visual = new Line();
+        super();
         this.material = new LineBasicMaterial({ color: lineColor });
     }
 
     public update(origin: Vector3, destination: Vector3): void {
-        const geometry: Geometry = new Geometry();
-        geometry.vertices.push(origin);
-        geometry.vertices.push(destination);
-        this.visual = new Line(geometry, this.material);
+        this.geometry = new Geometry();
+        this.geometry.vertices.push(origin);
+        this.geometry.vertices.push(destination);
     }
 }
