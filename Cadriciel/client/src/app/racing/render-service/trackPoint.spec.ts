5a6806f13abc1e6bc12183dc6535742141e5dd50
Anred <nissow301@gmail.com>
2018-02-24 20:14:34 -0500

    Track points tests created

--- Cadriciel/client/src/app/racing/render-service/trackPoint.spec.ts
+++ Cadriciel/client/src/app/racing/render-service/trackPoint.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, inject } from "@angular/core/testing";
-import { TrackPoint } from "./trackPoint";
+import { TrackPoint, TrackPointList } from "./trackPoint";
 // import { Object3D } from "three";
 import assert = require("assert");
+import { CommonCoordinate3D } from "../../../../../common/racing/commonCoordinate3D";
 
 describe("TrackPoint", () => {
-
+    const track: TrackPoint = new TrackPoint();
+    const trackList: TrackPointList = new TrackPointList(new Array<CommonCoordinate3D>());
     beforeEach(() => {
 
         TestBed.configureTestingModule({
@@ -17,6 +19,7 @@ describe("TrackPoint", () => {
     }));
 
     /*it("should accelerate when accelerator is pressed", () => {
+
         expect().toBeGreaterThan(initialSpeed);
     });*/
 });
