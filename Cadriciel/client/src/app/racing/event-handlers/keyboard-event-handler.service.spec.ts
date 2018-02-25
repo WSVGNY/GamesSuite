112903a9cb68a9b023a20efdd97154ee71e9166e
charlesnollet <chrlsnllt@gmail.com>
2018-02-25 18:48:38 -0500

    [CA-08] Debut des tests d'evenement de keyboard

--- Cadriciel/client/src/app/racing/event-handlers/keyboard-event-handler.service.spec.ts
+++ Cadriciel/client/src/app/racing/event-handlers/keyboard-event-handler.service.spec.ts
@@ -1,15 +1,28 @@
+/* tslint:disable: no-magic-numbers */
 import { TestBed, inject } from "@angular/core/testing";
 
 import { KeyboardEventHandlerService } from "./keyboard-event-handler.service";
+import { Car } from "../car/car";
+import { RaceGame } from "../game-loop/raceGame";
+import { RenderService } from "../render-service/render.service";
+
+const RACEGAME: RaceGame = new RaceGame(new RenderService());
 
 describe("KeyboardEventHandlerService", () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [KeyboardEventHandlerService]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [KeyboardEventHandlerService]
+        });
     });
-  });
 
-  it("should be created", inject([KeyboardEventHandlerService], (service: KeyboardEventHandlerService) => {
-    expect(service).toBeTruthy();
-  }));
+    it("should be created", inject([KeyboardEventHandlerService], (service: KeyboardEventHandlerService) => {
+        expect(service).toBeTruthy();
+    }));
+
+    it("should accelerate on key press", inject([KeyboardEventHandlerService], (service: KeyboardEventHandlerService) => {
+        service.handleKeyDown(87, RACEGAME);
+        expect(RACEGAME.playerCar["isAccelerating"]);
+
+    });
+
 });
