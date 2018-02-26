1a6e8cd5ffc9358861f14626d19c35ab3dce8fed
chbinc <chelsy.binet@polymtl.ca>
2018-02-26 13:23:06 -0500

    keyEvent tests

--- Cadriciel/client/src/app/racing/event-handlers/keyboard-event-handler.service.spec.ts
+++ Cadriciel/client/src/app/racing/event-handlers/keyboard-event-handler.service.spec.ts
@@ -7,6 +7,8 @@ import { RaceGame } from "../game-loop/raceGame";
 import { RenderService } from "../render-service/render.service";
 
 const RACEGAME: RaceGame = new RaceGame(new RenderService());
+const MS_BETWEEN_FRAMES: number = 16.6667;
+const MAXIMUM_STEERING_ANGLE: number = 0.15;
 
 describe("KeyboardEventHandlerService", () => {
     beforeEach(() => {
@@ -20,9 +22,27 @@ describe("KeyboardEventHandlerService", () => {
     }));
 
     it("should accelerate on key press", inject([KeyboardEventHandlerService], (service: KeyboardEventHandlerService) => {
-        service.handleKeyDown(87, RACEGAME);
-        expect(RACEGAME.playerCar["isAccelerating"]);
+        service.handleKeyDown(new KeyboardEvent("87"), RACEGAME);
+        expect(RACEGAME.playerCar["_isAcceleratorPressed"]);
+    }));
 
-    });
+    it("should break on key press", inject([KeyboardEventHandlerService], (service: KeyboardEventHandlerService) => {
+        service.handleKeyDown(new KeyboardEvent("83"), RACEGAME);
+        expect(RACEGAME.playerCar["_isBraking"]);
+    }));
+
+    it("should turn on key press", inject([KeyboardEventHandlerService], (service: KeyboardEventHandlerService) => {
+        service.handleKeyDown(new KeyboardEvent("65"), RACEGAME);
+        RACEGAME.playerCar.update(MS_BETWEEN_FRAMES);
+        expect(RACEGAME.playerCar["_steeringWheelDirection"]).toBeGreaterThan(0);
+
+    }));
+
+    it("the speed should increase on key hold", inject([KeyboardEventHandlerService], (service: KeyboardEventHandlerService) => {
+        const initialSpeed: number = RACEGAME.playerCar.speed.length();
+        service.handleKeyDown(new KeyboardEvent("87"), RACEGAME);
+        RACEGAME.playerCar.update(MS_BETWEEN_FRAMES);
+        expect(RACEGAME.playerCar.speed.length()).toBeGreaterThan(initialSpeed);
+    }));
 
 });
