c91b8b8f4ab0852d4e7bf61567358c42e9a4490d
charlesnollet <chrlsnllt@gmail.com>
2018-02-26 15:25:23 -0500

    [CA-08] Keyboard tests done

--- Cadriciel/client/src/app/racing/event-handlers/keyboard-event-handler.service.spec.ts
+++ Cadriciel/client/src/app/racing/event-handlers/keyboard-event-handler.service.spec.ts
@@ -2,47 +2,85 @@
 import { TestBed, inject } from "@angular/core/testing";
 
 import { KeyboardEventHandlerService } from "./keyboard-event-handler.service";
-import { Car } from "../car/car";
 import { RaceGame } from "../game-loop/raceGame";
 import { RenderService } from "../render-service/render.service";
+import { Car } from "../car/car";
+import { Vector3 } from "three";
 
-const RACEGAME: RaceGame = new RaceGame(new RenderService());
 const MS_BETWEEN_FRAMES: number = 16.6667;
-const MAXIMUM_STEERING_ANGLE: number = 0.15;
 
 describe("KeyboardEventHandlerService", () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [KeyboardEventHandlerService]
+            providers: [KeyboardEventHandlerService, RenderService]
         });
     });
 
-    it("should be created", inject([KeyboardEventHandlerService], (service: KeyboardEventHandlerService) => {
-        expect(service).toBeTruthy();
+    it("should be created", inject([KeyboardEventHandlerService], (keyboardService: KeyboardEventHandlerService) => {
+        expect(keyboardService).toBeTruthy();
     }));
 
-    it("should accelerate on key press", inject([KeyboardEventHandlerService], (service: KeyboardEventHandlerService) => {
-        service.handleKeyDown(new KeyboardEvent("87"), RACEGAME);
-        expect(RACEGAME.playerCar["_isAcceleratorPressed"]);
+    it("should be created", inject([RenderService], (renderService: RenderService) => {
+        expect(renderService).toBeTruthy();
     }));
 
-    it("should break on key press", inject([KeyboardEventHandlerService], (service: KeyboardEventHandlerService) => {
-        service.handleKeyDown(new KeyboardEvent("83"), RACEGAME);
-        expect(RACEGAME.playerCar["_isBraking"]);
-    }));
+    it(
+        "should accelerate on key press",
+        inject(
+            [KeyboardEventHandlerService, RenderService],
+            (service: KeyboardEventHandlerService, renderService: RenderService) => {
+                const raceGame: RaceGame = new RaceGame(renderService);
+                service.handleKeyDown(new KeyboardEvent("87"), raceGame);
+                expect(raceGame.playerCar["_isAcceleratorPressed"]);
+            }
+        )
+    );
 
-    it("should turn on key press", inject([KeyboardEventHandlerService], (service: KeyboardEventHandlerService) => {
-        service.handleKeyDown(new KeyboardEvent("65"), RACEGAME);
-        RACEGAME.playerCar.update(MS_BETWEEN_FRAMES);
-        expect(RACEGAME.playerCar["_steeringWheelDirection"]).toBeGreaterThan(0);
+    it(
+        "should break on key press",
+        inject(
+            [KeyboardEventHandlerService, RenderService],
+            (service: KeyboardEventHandlerService, renderService: RenderService) => {
+                const raceGame: RaceGame = new RaceGame(renderService);
+                service.handleKeyDown(new KeyboardEvent("83"), raceGame);
+                expect(raceGame.playerCar["_isBraking"]);
+            }
+        )
+    );
 
-    }));
+    it(
+        "should turn on key press",
+        inject(
+            [KeyboardEventHandlerService, RenderService],
+            (service: KeyboardEventHandlerService, renderService: RenderService) => {
+                const raceGame: RaceGame = new RaceGame(renderService);
+                service.handleKeyDown(new KeyboardEvent("65"), raceGame);
+                const car: Car = new Car();
+                car.init(new Vector3(0, 0, 0), 0).then(() => {
+                    raceGame["_playerCar"] = car;
+                    raceGame.playerCar.update(MS_BETWEEN_FRAMES);
+                    expect(raceGame.playerCar["_steeringWheelDirection"]).toBeGreaterThan(0);
+                });
+            }
+        )
+    );
 
-    it("the speed should increase on key hold", inject([KeyboardEventHandlerService], (service: KeyboardEventHandlerService) => {
-        const initialSpeed: number = RACEGAME.playerCar.speed.length();
-        service.handleKeyDown(new KeyboardEvent("87"), RACEGAME);
-        RACEGAME.playerCar.update(MS_BETWEEN_FRAMES);
-        expect(RACEGAME.playerCar.speed.length()).toBeGreaterThan(initialSpeed);
-    }));
+    it(
+        "the speed should increase on key hold",
+        inject(
+            [KeyboardEventHandlerService, RenderService],
+            (service: KeyboardEventHandlerService, renderService: RenderService) => {
+                const raceGame: RaceGame = new RaceGame(renderService);
+                const initialSpeed: number = raceGame.playerCar.speed.length();
+                service.handleKeyDown(new KeyboardEvent("87"), raceGame);
+                const car: Car = new Car();
+                car.init(new Vector3(0, 0, 0), 0).then(() => {
+                    raceGame["_playerCar"] = car;
+                    raceGame.playerCar.update(MS_BETWEEN_FRAMES);
+                    expect(raceGame.playerCar.speed.length()).toBeGreaterThan(initialSpeed);
+                });
+            }
+        )
+    );
 
 });
