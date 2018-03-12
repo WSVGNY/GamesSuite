e0de7c911c8bf9536e946ff9eae70472b229c5f0
wisev <wsvgny@gmail.com>
2018-03-12 18:08:45 -0400

    [REFACTOR] Migrated test files for editor and preview scenes

---
+++ Cadriciel/client/src/app/racing/scenes/editorScene.spec.ts
@@ -0,0 +1,132 @@
+import { EditorScene, VERTEX_GEOMETRY, SIMPLE_VERTEX_MATERIAL, SIMPLE_LINE_MATERIAL } from "./editorScene";
+import { Vector3, Scene, Mesh, Geometry, Line } from "three";
+
+// tslint:disable:no-magic-numbers
+describe("Editor Scene", () => {
+
+    let editorScene: EditorScene;
+
+    beforeEach(() => {
+        editorScene = new EditorScene();
+    });
+
+    it("should be instantiable", () => {
+        expect(editorScene).toBeTruthy();
+    });
+
+    it("should create Mesh vertex with createVertex()", () => {
+        const position: Vector3 = new Vector3(0, 0, 0);
+        const vertex: Mesh = editorScene["createVertex"](position);
+        expect(vertex).toBeTruthy();
+    });
+
+    it("should add vertex at the right position with addVertex()", () => {
+        const position: Vector3 = new Vector3(0, 0, 0);
+        editorScene.addVertex(position);
+        const receivedPosition: Vector3 = editorScene["_scene"].getChildByName("Start").position;
+        expect(position).toEqual(receivedPosition);
+    });
+
+    it("should keep first entered vertex as firstVertex when adding a new point", () => {
+        const position: Vector3 = new Vector3(0, 0, 0);
+        const position2: Vector3 = new Vector3(0, 1, 0);
+        editorScene.addVertex(position);
+        const first: Mesh = editorScene["_firstVertex"];
+        editorScene.addVertex(position2);
+        expect(editorScene["_firstVertex"]).toEqual(first);
+    });
+
+    it("should remove the last point added to the scene", () => {
+        const testScene: Scene = new Scene();
+        const vertex: Mesh = new Mesh(VERTEX_GEOMETRY, SIMPLE_VERTEX_MATERIAL);
+        vertex.position.set(0, 0, 0);
+        testScene.add(vertex);
+        editorScene["_vertices"].push(vertex);
+        editorScene["_scene"] = testScene;
+        editorScene.removeLastVertex();
+        expect(testScene.children.length).toBeFalsy();
+    });
+
+    it("should update lastVertex when adding a point", () => {
+        const position: Vector3 = new Vector3(0, 0, 0);
+        editorScene.addVertex(position);
+        const last: Mesh = editorScene["_firstVertex"];
+        expect(editorScene["_lastVertex"]).toEqual(last);
+    });
+
+    it("should update lastVertex when removing a point", () => {
+        const testScene: Scene = new Scene();
+        const vertex1: Mesh = new Mesh(VERTEX_GEOMETRY, SIMPLE_VERTEX_MATERIAL);
+        const vertex2: Mesh = new Mesh(VERTEX_GEOMETRY, SIMPLE_VERTEX_MATERIAL);
+        vertex1.position.set(0, 0, 0);
+        vertex2.position.set(0, 1, 0);
+        testScene.add(vertex1);
+        testScene.add(vertex2);
+        editorScene["_vertices"].push(vertex1);
+        editorScene["_vertices"].push(vertex2);
+        editorScene["_scene"] = testScene;
+        editorScene.removeLastVertex();
+        expect(editorScene["_lastVertex"]).toEqual(vertex1);
+    });
+
+    it("should create a connection between two points", () => {
+        const vertex0: Mesh = new Mesh(VERTEX_GEOMETRY, SIMPLE_VERTEX_MATERIAL);
+        const vertex1: Mesh = new Mesh(VERTEX_GEOMETRY, SIMPLE_VERTEX_MATERIAL);
+        vertex0.position.set(0, 0, 0);
+        vertex1.position.set(1, 1, 0);
+        editorScene.addConnection(vertex0, vertex1);
+        expect(editorScene["_scene"].getChildByName("connection0")).toBeDefined();
+    });
+
+    it("should remove the last connection added to the scene", () => {
+        const LINE_GEOMETRY: Geometry = new Geometry();
+        LINE_GEOMETRY.vertices.push(new Vector3(0, 0, 0));
+        LINE_GEOMETRY.vertices.push(new Vector3(1, 1, 0));
+        const connection: Line = new Line(LINE_GEOMETRY, SIMPLE_LINE_MATERIAL);
+        editorScene["_connections"].push(connection);
+        const testScene: Scene = new Scene();
+        testScene.add(connection);
+        editorScene["_scene"] = testScene;
+        editorScene.removeLastVertex();
+        expect(editorScene["_scene"].children.length).toBeFalsy();
+    });
+
+    it("should update the position of a dragged point", () => {
+        const vertex: Mesh = new Mesh(VERTEX_GEOMETRY, SIMPLE_VERTEX_MATERIAL);
+        vertex.position.set(0, 0, 0);
+        vertex.name = "vertex0";
+        editorScene["_vertices"].push(vertex);
+        const testScene: Scene = new Scene();
+        testScene.add(vertex);
+        editorScene["_scene"] = testScene;
+        const VXY: number = 2;
+        editorScene.setVertexPosition(editorScene["_scene"].getObjectByName("vertex0") as Mesh, new Vector3(VXY, VXY, 0));
+        expect(editorScene["_scene"].getObjectByName("vertex0").position).toEqual(new Vector3(VXY, VXY, 0));
+    });
+
+    it("should set _isComplete to true upon completition of the track", () => {
+        const vertex0: Mesh = new Mesh(VERTEX_GEOMETRY, SIMPLE_VERTEX_MATERIAL);
+        const vertex1: Mesh = new Mesh(VERTEX_GEOMETRY, SIMPLE_VERTEX_MATERIAL);
+        vertex0.position.set(0, 0, 0);
+        vertex1.position.set(0, 20, 0);
+        editorScene["_vertices"].push(vertex0);
+        editorScene["_vertices"].push(vertex1);
+        editorScene["_firstVertex"] = vertex0;
+        editorScene["_lastVertex"] = vertex1;
+        editorScene.completeTrack();
+        expect(editorScene["_isComplete"]).toEqual(true);
+    });
+
+    it("should create a connection between the last and first vertex upon completition of the track", () => {
+        const vertex0: Mesh = new Mesh(VERTEX_GEOMETRY, SIMPLE_VERTEX_MATERIAL);
+        const vertex1: Mesh = new Mesh(VERTEX_GEOMETRY, SIMPLE_VERTEX_MATERIAL);
+        vertex0.position.set(0, 0, 0);
+        vertex1.position.set(0, 20, 0);
+        editorScene["_vertices"].push(vertex0);
+        editorScene["_vertices"].push(vertex1);
+        editorScene["_firstVertex"] = vertex0;
+        editorScene["_lastVertex"] = vertex1;
+        editorScene.completeTrack();
+        expect(editorScene["_scene"].getChildByName("connection0")).toBeDefined();
+    });
+});
---
+++ Cadriciel/client/src/app/racing/scenes/previewScene.spec.ts
@@ -0,0 +1,28 @@
+// tslint:disable:no-magic-numbers
+
+import { TestBed, inject } from "@angular/core/testing";
+import { RenderService } from "../render-service/render.service";
+import { PreviewScene } from "./previewScene";
+// import { TrackStructure } from "../../../../../common/racing/track";
+// import { Track } from "../track";
+
+describe("Preview Scene", () => {
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [RenderService]
+        });
+    });
+
+    it("should be created", inject([RenderService], (renderService: RenderService) => {
+        const previewScene: PreviewScene = new PreviewScene();
+        expect(previewScene).toBeTruthy();
+    }));
+
+    // it("should add the desired track to the preview", inject([RenderService], (renderService: RenderService) => {
+    //     const trackPreview: TrackPreview = new TrackPreview(renderService);
+    //     const mockTrack: Track = new Track(TrackStructure.getNewDefaultTrackStructure());
+    //     trackPreview.loadTrack(mockTrack);
+    //     expect(renderService["_scene"].getChildByName("track")).toBeDefined();
+    // }));
+});
