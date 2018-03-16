4aee3cab79a35fccfe6d4c24f8ee38fc0435285f
wisev <wsvgny@gmail.com>
2018-03-16 10:12:54 -0400

    [REFACTOR] Temporarily disabled debug features

--- Cadriciel/client/src/app/racing/event-handlers/keyboard-event-handler.service.ts
+++ Cadriciel/client/src/app/racing/event-handlers/keyboard-event-handler.service.ts
@@ -7,15 +7,16 @@ import { Brake } from "../commands/carAICommands/brake";
 import { ReleaseAccelerator } from "../commands/carAICommands/releaseAccelerator";
 import { ReleaseSteering } from "../commands/carAICommands/releaseSteering";
 import { ReleaseBrakes } from "../commands/carAICommands/releaseBrakes";
-import { RaceGame } from "../game-loop/raceGame";
+// import { RaceGame } from "../game-loop/raceGame";
+import { Car } from "../car/car";
 
 const ACCELERATE_KEYCODE: number = 87;  // w
 const LEFT_KEYCODE: number = 65;        // a
 const BRAKE_KEYCODE: number = 83;       // s
 const RIGHT_KEYCODE: number = 68;       // d
-const DAY_KEYCODE: number = 74;         // j
-const NIGHT_KEYCODE: number = 78;       // n
-const DEBUG_KEYCODE: number = 48;       // 0
+// const DAY_KEYCODE: number = 74;         // j
+// const NIGHT_KEYCODE: number = 78;       // n
+// const DEBUG_KEYCODE: number = 48;       // 0
 
 @Injectable()
 export class KeyboardEventHandlerService {
@@ -26,51 +27,51 @@ export class KeyboardEventHandlerService {
         this._carControl = new CommandController();
     }
 
-    public handleKeyDown(event: KeyboardEvent, raceGame: RaceGame): void {
+    public handleKeyDown(event: KeyboardEvent, car: Car): void {
         switch (event.keyCode) {
             case ACCELERATE_KEYCODE:
-                this._carControl.command = new GoFoward(raceGame.playerCar);
+                this._carControl.command = new GoFoward(car);
                 this._carControl.execute();
                 break;
             case LEFT_KEYCODE:
-                this._carControl.command = new TurnLeft(raceGame.playerCar);
+                this._carControl.command = new TurnLeft(car);
                 this._carControl.execute();
                 break;
             case RIGHT_KEYCODE:
-                this._carControl.command = new TurnRight(raceGame.playerCar);
+                this._carControl.command = new TurnRight(car);
                 this._carControl.execute();
                 break;
             case BRAKE_KEYCODE:
-                this._carControl.command = new Brake(raceGame.playerCar);
+                this._carControl.command = new Brake(car);
                 this._carControl.execute();
                 break;
-            case DAY_KEYCODE:
-                raceGame.isDay = true;
-                break;
-            case NIGHT_KEYCODE:
-                raceGame.isDay = false;
-                break;
-            case DEBUG_KEYCODE:
-                raceGame.debug = !raceGame.debug;
-                break;
+            // case DAY_KEYCODE:
+            //     raceGame.isDay = true;
+            //     break;
+            // case NIGHT_KEYCODE:
+            //     raceGame.isDay = false;
+            //     break;
+            // case DEBUG_KEYCODE:
+            //     raceGame.debug = !raceGame.debug;
+            //     break;
             default:
                 break;
         }
     }
 
-    public handleKeyUp(event: KeyboardEvent, raceGame: RaceGame): void {
+    public handleKeyUp(event: KeyboardEvent, car: Car): void {
         switch (event.keyCode) {
             case ACCELERATE_KEYCODE:
-                this._carControl.command = new ReleaseAccelerator(raceGame.playerCar);
+                this._carControl.command = new ReleaseAccelerator(car);
                 this._carControl.execute();
                 break;
             case LEFT_KEYCODE:
             case RIGHT_KEYCODE:
-                this._carControl.command = new ReleaseSteering(raceGame.playerCar);
+                this._carControl.command = new ReleaseSteering(car);
                 this._carControl.execute();
                 break;
             case BRAKE_KEYCODE:
-                this._carControl.command = new ReleaseBrakes(raceGame.playerCar);
+                this._carControl.command = new ReleaseBrakes(car);
                 this._carControl.execute();
                 break;
             default:
