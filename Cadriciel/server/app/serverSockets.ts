5d40622813845504d63bdce7a3b3797704ebc1e6
silegc <simon.legault@polymtl.ca>
2018-03-16 12:27:08 -0400

    [MC-08] kind of a room implementation

--- Cadriciel/server/app/serverSockets.ts
+++ Cadriciel/server/app/serverSockets.ts
@@ -3,11 +3,11 @@ import { SocketEvents } from "../../common/communication/socketEvents";
 import * as http from "http";
 
 export class ServerSockets {
+    private static _numberOfRoom: number = 0;
     private readonly baseRoomName: string = "ROOM";
 
     private io: SocketIO.Server;
-    private _roomName: string = this.baseRoomName;
-    private _numberOfRoom: number = 0;
+    public _roomName: string = this.baseRoomName;
     private _httpServer: http.Server;
 
     public constructor(server: http.Server, initialize: boolean = false) {
@@ -22,12 +22,14 @@ export class ServerSockets {
         this.io = sio(this._httpServer);
         this.io.on(SocketEvents.Connection, (socket: SocketIO.Socket) => {
             if (this._roomName !== this.baseRoomName) {
+                this.createRoom();
+                console.log(this._roomName);
                 socket.join(this._roomName);
             }
             console.log("user connected");
             socket.on(SocketEvents.NewMessage, (message: string) => {
                 console.log(message);
-                this.io.emit(SocketEvents.NewMessage, message);
+                this.io.to(this._roomName).emit(SocketEvents.NewMessage, message);
             });
             socket.on(SocketEvents.Disconnection, () => {
                 console.log("user disconnected");
@@ -37,7 +39,8 @@ export class ServerSockets {
     // tslint:enable:no-console
 
     public createRoom(): void {
-        this._roomName += this._numberOfRoom++;
+        this._roomName = this.baseRoomName;
+        this._roomName += ServerSockets._numberOfRoom++;
 
     }
 
