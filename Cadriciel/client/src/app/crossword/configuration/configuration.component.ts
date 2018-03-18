22f717dce34a2096a237b5f6a1b5ec6c50a7b88e
chbinc <chelsy.binet@polymtl.ca>
2018-03-18 17:35:01 -0400

    faire en sorte que les grilles generent

--- Cadriciel/client/src/app/crossword/configuration/configuration.component.ts
+++ Cadriciel/client/src/app/crossword/configuration/configuration.component.ts
@@ -50,7 +50,7 @@ export class ConfigurationComponent {
 
     private makeGrid(): void {
         this.choseGridDifficulty = true;
-        // this.createGrid();
+        this.createGrid();
     }
 
     public makeEasyGrid(): void {
