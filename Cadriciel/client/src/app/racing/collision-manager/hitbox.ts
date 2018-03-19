54f9638bb013e3f49a1cd67038104f174c121eee
wisev <wsvgny@gmail.com>
2018-03-19 15:55:49 -0400

    [CA-19] [WIP] Changed hitbox to a single plane to optimize calculations

--- Cadriciel/client/src/app/racing/collision-manager/hitbox.ts
+++ Cadriciel/client/src/app/racing/collision-manager/hitbox.ts
@@ -1,19 +1,22 @@
-import { Mesh, BoxGeometry, MeshBasicMaterial, Vector3 } from "three";
+import { Mesh, /*BoxGeometry,*/ MeshBasicMaterial, Vector3, PlaneGeometry } from "three";
 
 const WIDTH: number = 1.5;
-const HEIGHT: number = 1;
+// const HEIGHT: number = 0.01;
 const DEPTH: number = 3.1;
 
 export class Hitbox extends Mesh {
 
     public constructor() {
-        const geometry: BoxGeometry = new BoxGeometry(WIDTH, HEIGHT, DEPTH);
-        geometry.computeBoundingSphere();
-        const material: MeshBasicMaterial = new MeshBasicMaterial({ color: 0x00FF00 });
+        const geometry: PlaneGeometry = new PlaneGeometry(WIDTH, DEPTH);
+        geometry.verticesNeedUpdate = true;
+        // geometry.applyMatrix();
+        const material: MeshBasicMaterial = new MeshBasicMaterial({wireframe: true, color: 0x00FF00 });
         super(geometry, material);
+        (this.geometry as PlaneGeometry).verticesNeedUpdate = true;
+        console.log((this.geometry as PlaneGeometry).vertices);
     }
 
     public get vertices(): Vector3[] {
-        return (this.geometry as BoxGeometry).vertices;
+        return (this.geometry as PlaneGeometry).vertices;
     }
 }
