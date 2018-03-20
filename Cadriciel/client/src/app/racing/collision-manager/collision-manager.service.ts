c249430a285428b085f492b01a3f6dfaea5d3edf
wisev <wsvgny@gmail.com>
2018-03-19 22:28:23 -0400

    [CA-19] Basic elastic collisions are implemented

--- Cadriciel/client/src/app/racing/collision-manager/collision-manager.service.ts
+++ Cadriciel/client/src/app/racing/collision-manager/collision-manager.service.ts
@@ -1,88 +1,73 @@
 import { Injectable } from "@angular/core";
 import { Car } from "../car/car";
 import { Vector3, Raycaster, Intersection } from "three";
-import { VectorHelper } from "../artificial-intelligence/vectorHelper";
+// import { VectorHelper } from "../artificial-intelligence/vectorHelper";
 import { GameScene } from "../scenes/gameScene";
 
-const MINIMUM_CAR_DISTANCE: number = 5;
+const MINIMUM_CAR_DISTANCE: number  = 5;
 @Injectable()
 export class CollisionManagerService {
 
-    private collisionAxisHelper: VectorHelper;
-    private collisionAxisOrthoHelper: VectorHelper;
-    private collisionToFirstCarHelper: VectorHelper;
-    private collisionToSecondCarHelper: VectorHelper;
+    // private collisionAxisHelper: VectorHelper;
+    // private collisionAxisOrthoHelper: VectorHelper;
+    // private collisionToFirstCarHelper: VectorHelper;
+    // private collisionToSecondCarHelper: VectorHelper;
 
     public constructor() {
         // tslint:disable-next-line:no-magic-numbers
-        this.collisionAxisHelper = new VectorHelper(0xFFFFFF);
-        this.collisionToFirstCarHelper = new VectorHelper(0x0000FF);
-        this.collisionToSecondCarHelper = new VectorHelper(0xFF0000);
-        this.collisionAxisOrthoHelper = new VectorHelper(0xFF00FF);
+        // this.collisionAxisHelper = new VectorHelper(0xFFFFFF);
+        // this.collisionToFirstCarHelper = new VectorHelper(0x0000FF);
+        // this.collisionToSecondCarHelper = new VectorHelper(0xFF0000);
+        // this.collisionAxisOrthoHelper = new VectorHelper(0xFF00FF);
     }
 
-    // tslint:disable-next-line:max-func-body-length
     public computeCollisions(cars: Car[], gameScene: GameScene): void {
         for (let i: number = 0; i < cars.length; ++i) {
             for (let j: number = i + 1; j < cars.length; ++j) {
                 if (cars[i].currentPosition.distanceTo(cars[j].currentPosition) < MINIMUM_CAR_DISTANCE) {
-                    // tslint:disable-next-line:prefer-for-of
-                    for (let vertexIndex: number = 0; vertexIndex < cars[i].hitbox.subPlanVertices.length; ++vertexIndex) {
-                        const localVertex: Vector3 = cars[i].hitbox.subPlanVertices[vertexIndex].clone();
-                        const globalVertex: Vector3 = localVertex.applyMatrix4(cars[i].meshMatrix);
-                        const direction: Vector3 = globalVertex.sub(cars[i].currentPosition);
-                        const ray: Raycaster = new Raycaster(cars[i].currentPosition.clone(), direction.clone().normalize());
-                        const collisionResult: Intersection[] = ray.intersectObject(cars[j].hitbox);
-                        if (collisionResult.length > 0 && collisionResult[0].distance < direction.length()) {
-                            gameScene.remove(this.collisionToFirstCarHelper);
-                            gameScene.remove(this.collisionToSecondCarHelper);
-                            const collisionAxis: Vector3 = this.computeCollisionAxis(cars[i], cars[j], collisionResult[0].point);
-                            const collisionAxisOrtho: Vector3 = new Vector3(
-                                -collisionAxis.z,
-                                0,
-                                collisionAxis.x
-                            );
-                            gameScene.add(this.collisionToFirstCarHelper);
-                            gameScene.add(this.collisionToSecondCarHelper);
-                            gameScene.remove(this.collisionAxisHelper);
-                            gameScene.remove(this.collisionAxisOrthoHelper);
-                            this.collisionAxisHelper.update(collisionResult[0].point.clone(),
-                                                            collisionResult[0].point.clone().add(collisionAxis.clone().multiplyScalar(10)));
-                            this.collisionAxisOrthoHelper.update(collisionResult[0].point.clone(),
-                                                                 collisionResult[0].point.clone().add(
-                                                                     collisionAxisOrtho.clone().multiplyScalar(10)));
-                            gameScene.add(this.collisionAxisHelper);
-                            gameScene.add(this.collisionAxisOrthoHelper);
-                        }
+                    const collisionPoint: Vector3 = this.detectCollision(cars[i], cars[j]);
+                    if (collisionPoint !== undefined) {
+                        const collisionAxis: Vector3 = this.computeCollisionAxis(cars[i], cars[j], collisionPoint);
+                        const collisionAxisOrtho: Vector3 = new Vector3(collisionAxis.z, 0, - collisionAxis.x);
+                        // const tangencialComponent: Vector3 = cars[i]._speed.clone().projectOnVector(collisionAxis);
+                        const radialComponent: Vector3 = cars[i]._speed.clone().projectOnVector(collisionAxisOrtho);
+
+                        cars[i]._speed.sub(radialComponent);
+                        cars[j]._speed.add(radialComponent);
                     }
                 }
             }
         }
     }
 
+    private detectCollision(firstCar: Car, secondCar: Car): Vector3 {
+        for (const vertex of firstCar.hitbox.subPlanVertices) {
+            const localVertex: Vector3 = vertex.clone();
+            const globalVertex: Vector3 = localVertex.applyMatrix4(firstCar.meshMatrix);
+            const direction: Vector3 = globalVertex.sub(firstCar.currentPosition);
+            const ray: Raycaster = new Raycaster(firstCar.currentPosition.clone(), direction.clone().normalize());
+            const collisionResult: Intersection[] = ray.intersectObject(secondCar.hitbox);
+            if (collisionResult.length > 0 && collisionResult[0].distance < direction.length()) {
+                return collisionResult[0].point;
+            }
+        }
+
+        return undefined;
+    }
+
     private computeCollisionAxis(firstCar: Car, secondCar: Car, collisionPoint: Vector3): Vector3 {
         const collisionToFirstCar: Vector3 = firstCar.currentPosition.clone().sub(collisionPoint);
-        collisionToFirstCar.y = 0;
         const collisionToSecondCar: Vector3 = secondCar.currentPosition.clone().sub(collisionPoint);
+        collisionToFirstCar.y = 0;
         collisionToSecondCar.y = 0;
-
-        this.collisionToFirstCarHelper.update(firstCar.currentPosition.clone().add(new Vector3(0, 0.1, 0)),
-                                              collisionPoint.clone().add(new Vector3(0, 0.1, 0)));
-        this.collisionToSecondCarHelper.update(secondCar.currentPosition.clone().add(new Vector3(0, 0.1, 0)),
-                                               collisionPoint.clone().add(new Vector3(0, 0.1, 0)));
 
         let halfOfSmallAngle: number = collisionToFirstCar.angleTo(collisionToSecondCar) / 2;
         if (collisionToFirstCar.clone().cross(collisionToSecondCar).y < 0) {
             halfOfSmallAngle = -halfOfSmallAngle;
         }
-
         const collisionAxis: Vector3 = collisionToFirstCar.clone().normalize().applyAxisAngle(new Vector3(0, 1, 0), halfOfSmallAngle);
         collisionAxis.y = 0;
 
         return collisionAxis;
     }
-
-    // private computeResultingForces(motionLessCar: Car, movingCar: Car, collisionPoint: Vector3): void {
-
-    // }
 }
