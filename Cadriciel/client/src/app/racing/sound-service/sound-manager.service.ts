dbe5e9ee755da589ba35a2db8fccec5f9ed7f210
Anred <nissow301@gmail.com>
2018-03-20 12:45:19 -0400

    [CA-20] Another sound refactor

--- Cadriciel/client/src/app/racing/sound-service/sound-manager.service.ts
+++ Cadriciel/client/src/app/racing/sound-service/sound-manager.service.ts
@@ -9,80 +9,17 @@ export class SoundManagerService {
 
   private _music: Audio;
   private _soundEffect: Audio;
-  private _isPlaying: boolean = true;
+  private _isPlayingAcceleration: boolean = true;
   private _isDetected: boolean = true;
   private _isPlayingMusic: boolean = true;
   private _collisionSound: Audio;
 
-  public createMusic(soundName: string, car: Car): void {
+  private createSound(soundName: string): Audio {
     const listener: AudioListener = new AudioListener();
-    const music: Audio = new Audio(listener); // Maybe positionnal audio
-    const loader: AudioLoader = new AudioLoader();
-    loader.load(
-      soundName,
-      (audioBuffer: AudioBuffer) => {
-        music.setBuffer(audioBuffer);
-        music.play();
-      },
-      (xhr: XMLHttpRequest) => { },
-      (err: Event) => { }
-    );
-    car.add(music);
-    this._music = music;
-    this._isPlayingMusic = false;
-    /*for (let i: number = 0; i < RaceGameConfig.AI_CARS_NUMBER; ++i) { // Pour ajouter aux IA
-        this._aiCars[i].add(sound);
-    }*/
-  }
-
-  public createAccelerationEffect(effectName: string, car: Car): void {
-    const listener: AudioListener = new AudioListener();
-    const soundEffect: Audio = new Audio(listener); // Maybe positionnal audio
-    const loader: AudioLoader = new AudioLoader();
-    loader.load(
-      effectName,
-      (audioBuffer: AudioBuffer) => {
-        soundEffect.setBuffer(audioBuffer);
-        soundEffect.play();
-      },
-      (xhr: XMLHttpRequest) => { },
-      (err: Event) => { }
-    );
-    car.add(soundEffect);
-    this._soundEffect = soundEffect;
-    this._isPlaying = false;
-  }
-
-  public createCollisionSound(soundName: string, camera: PerspectiveCamera, car: Car): void {
-    const listener: AudioListener = new AudioListener();
-    camera.add(listener);
     const sound: Audio = new Audio(listener); // Maybe positionnal audio
     const loader: AudioLoader = new AudioLoader();
     loader.load(
       soundName,
-      (audioBuffer: AudioBuffer) => {
-        sound.setBuffer(audioBuffer);
-        sound.stop();
-      },
-      (xhr: XMLHttpRequest) => { },
-      (err: Event) => { }
-    );
-    car.add(sound);
-    camera.add(sound);
-    this._isDetected = false;
-    this._collisionSound = sound;
-    /*for (let i: number = 0; i < RaceGameConfig.AI_CARS_NUMBER; ++i) { // Pour ajouter aux IA
-        this._aiCars[i].add(sound);
-    }*/
-  }
-
-  public createStartingSound(camera: PerspectiveCamera): void {
-    const listener: AudioListener = new AudioListener();
-    camera.add(listener);
-    const sound: Audio = new Audio(listener); // Maybe positionnal audio
-    const loader: AudioLoader = new AudioLoader();
-    loader.load(
-      "../../assets/sounds/startingSound.mp3",
       (audioBuffer: AudioBuffer) => {
         sound.setBuffer(audioBuffer);
         sound.play();
@@ -90,7 +27,34 @@ export class SoundManagerService {
       (xhr: XMLHttpRequest) => { },
       (err: Event) => { }
     );
+
+    return sound;
+  }
+
+  public createMusic( car: Car): void {
+    const music: Audio = this.createSound("../../assets/sounds/rainbowRoad.mp3");
+    car.add(music);
+    this._music = music;
+    this._isPlayingMusic = false;
+  }
+
+  public createAccelerationEffect( car: Car): void {
+    const soundEffect: Audio = this.createSound("../../assets/sounds/carAcceleration.mp3");
+    car.add(soundEffect);
+    this._soundEffect = soundEffect;
+    this._isPlayingAcceleration = false;
+  }
+
+  public createCollisionSound( camera: PerspectiveCamera, car: Car): void {
+    const sound: Audio = this.createSound("../../assets/sounds/collision-sound.mp3");
+    car.add(sound);
     camera.add(sound);
+    this._isDetected = false;
+    this._collisionSound = sound;
+  }
+
+  public createStartingSound(camera: PerspectiveCamera): void {
+    camera.add(this.createSound("../../assets/sounds/startingSound.mp3"));
   }
 
   public play (sound: Audio): void {
@@ -112,7 +76,7 @@ export class SoundManagerService {
   public get collisionSound(): Audio {
     return this._collisionSound;
   }
-  public isPlaying(): boolean { return this._isPlaying; }
+  public isPlaying(): boolean { return this._isPlayingAcceleration; }
   public isDetected(): boolean { return this._isDetected; }
   public isPlayingMusic(): boolean { return this._isPlayingMusic; }
 }
