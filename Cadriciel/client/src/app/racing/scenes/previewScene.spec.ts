f416865316dfdec822d000d748202a25fb36b53c
wisev <wsvgny@gmail.com>
2018-03-26 20:36:41 -0400

    [REVIEW] Added previewScene test for loading a track preview

--- Cadriciel/client/src/app/racing/scenes/previewScene.spec.ts
+++ Cadriciel/client/src/app/racing/scenes/previewScene.spec.ts
@@ -1,26 +1,32 @@
 // tslint:disable:no-magic-numbers
 
-import { TestBed, inject } from "@angular/core/testing";
 import { RenderService } from "../render-service/render.service";
 import { PreviewScene } from "./previewScene";
+import { CommonCoordinate3D } from "../../../../../common/racing/commonCoordinate3D";
+import { Track } from "../../../../../common/racing/track";
 
 describe("Preview Scene", () => {
 
+    let previewScene: PreviewScene;
+
     beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [RenderService]
-        });
+        previewScene = new PreviewScene();
     });
 
-    it("should be created", inject([RenderService], (renderService: RenderService) => {
-        const previewScene: PreviewScene = new PreviewScene();
+    it("should be created", () => {
         expect(previewScene).toBeTruthy();
-    }));
+    });
 
-    // it("should add the desired track to the preview", inject([RenderService], (renderService: RenderService) => {
-    //     const trackPreview: TrackPreview = new TrackPreview(renderService);
-    //     const mockTrack: Track = new Track(TrackStructure.getNewDefaultTrackStructure());
-    //     trackPreview.loadTrack(mockTrack);
-    //     expect(renderService["_scene"].getChildByName("track")).toBeDefined();
-    // }));
+    it("should add the desired track to the preview", () => {
+        const MOCK_TRACK: CommonCoordinate3D[] = [
+            new CommonCoordinate3D(0, 0, 0),
+            new CommonCoordinate3D(100, 0, 0),
+            new CommonCoordinate3D(100, 0, 100),
+            new CommonCoordinate3D(0, 0, 100),
+        ];
+        const mockTrack: Track = new Track("");
+        mockTrack.vertices = MOCK_TRACK;
+        previewScene.loadTrack(mockTrack);
+        expect(previewScene.getChildByName("track")).toBeDefined();
+    });
 });
