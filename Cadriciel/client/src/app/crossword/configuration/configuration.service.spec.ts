c4074b329386cb2bc162d74cf6cdd43b93e952b4
chbinc <chelsy.binet@polymtl.ca>
2018-03-26 20:15:34 -0400

    [TEST] finalisation tests crossword

--- Cadriciel/client/src/app/crossword/configuration/configuration.service.spec.ts
+++ Cadriciel/client/src/app/crossword/configuration/configuration.service.spec.ts
@@ -1,5 +1,14 @@
 import { TestBed, inject } from "@angular/core/testing";
 import { ConfigurationService } from "./configuration.service";
+import { Player } from "../../../../../common/crossword/player";
+
+const createMockPlayer: Function = (colorString: string, nameString: string, scoreNumber: number) => {
+    return {
+        color: colorString,
+        name: nameString,
+        score: scoreNumber
+    } as Player;
+};
 
 describe("ConfigurationService", () => {
     beforeEach(() => {
@@ -13,14 +22,18 @@ describe("ConfigurationService", () => {
     }));
 
     it("the current player is created", inject([ConfigurationService], (service: ConfigurationService) => {
-        expect(false).toBeTruthy();
+        service.playerOne = createMockPlayer("color", "name", 0);
+        expect(service.playerOne).toBeTruthy();
     }));
 
     it("the other player is created", inject([ConfigurationService], (service: ConfigurationService) => {
-        expect(false).toBeTruthy();
+        service.playerTwo = createMockPlayer("color", "name", 0);
+        expect(service.playerTwo).toBeTruthy();
     }));
 
     it("the current player and the other player are different", inject([ConfigurationService], (service: ConfigurationService) => {
-        expect(false).toBeTruthy();
+        service.playerOne = createMockPlayer("color", "name", 0);
+        service.playerTwo = createMockPlayer("color", "name", 0);
+        expect(service.playerOne === service.playerTwo).toBeFalsy();
     }));
 });
