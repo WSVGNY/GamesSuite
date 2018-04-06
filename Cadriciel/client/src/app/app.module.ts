897c70b65ef00b33ce2db336abcbddd2443c0114
charlesnollet <chrlsnllt@gmail.com>
2018-04-06 12:14:18 -0400

    [CA-12] best time table is pretty

--- Cadriciel/client/src/app/app.module.ts
+++ Cadriciel/client/src/app/app.module.ts
@@ -21,7 +21,7 @@ import { SoundManagerService } from "./racing/sound-service/sound-manager.servic
 import { MultiplayerCommunicationService } from "./crossword/multiplayer-communication.service";
 import { CollisionManagerService } from "./racing/collision-manager/collision-manager.service";
 import { CameraManagerService } from "./racing/cameras/camera-manager.service";
-import { BestTimesComponent } from './racing/best-times/best-times.component';
+import { BestTimesComponent } from "./racing/best-times/best-times.component";
 
 @NgModule({
     declarations: [
