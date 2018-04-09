dd9c2741f5e25001113d4a79b70602a962d5630c
charlesnollet <chrlsnllt@gmail.com>
2018-04-09 13:55:06 -0400

    [CA-12] Added leaderboard call method

--- Cadriciel/client/src/app/racing/scoreboard/input-time/input-time.component.ts
+++ Cadriciel/client/src/app/racing/scoreboard/input-time/input-time.component.ts
@@ -1,10 +1,13 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: "app-input-time",
-  templateUrl: "./input-time.component.html",
-  styleUrls: ["./input-time.component.css"]
+    selector: "app-input-time",
+    templateUrl: "./input-time.component.html",
+    styleUrls: ["./input-time.component.css"]
 })
 export class InputTimeComponent {
 
+    public loadLeaderBoard(name: string): void {
+
+    }
 }
