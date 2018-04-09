ad56f3cc93583464997455bb42e0abfadc1ec93e
Anred <nissow301@gmail.com>
2018-04-09 16:49:59 -0400

    [CA-10] Creating another carTracking manager service

---
+++ Cadriciel/client/src/app/racing/carTracking-manager/car-tracking-manager.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from "@angular/core/testing";
+
+import { CarTrackingManagerService } from "./car-tracking-manager.service";
+
+describe("CarTrackingManagerService", () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [CarTrackingManagerService]
+    });
+  });
+
+  it("should be created", inject([CarTrackingManagerService], (service: CarTrackingManagerService) => {
+    expect(service).toBeTruthy();
+  }));
+});
---
+++ Cadriciel/client/src/app/racing/carTracking-manager/car-tracking-manager.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from "@angular/core";
+import { TrackingSphere } from "./sphereDetection";
+import { Vector3 } from "three";
+import { CommonCoordinate3D } from "../../../../../common/racing/commonCoordinate3D";
+import { Car } from "../car/car";
+
+@Injectable()
+export class CarTrackingManagerService {
+
+    private _detectionSpheres: TrackingSphere[];
+    private _trackVertices: Vector3[];
+
+    public constructor() {
+        this._detectionSpheres = [];
+        this._trackVertices = [];
+    }
+
+    public init(trackVertices: CommonCoordinate3D[], car: Car): void {
+        this._trackVertices = [];
+        trackVertices.forEach((coordinate: CommonCoordinate3D) => {
+            this._trackVertices.push(new Vector3(coordinate.x, coordinate.y, coordinate.z));
+        });
+        this.createDetectionSpheres();
+        car.trackPortionIndex = 0;
+    }
+
+    private resetDetectionSpheres(): void {
+        for (let i: number = 0; i < this._trackVertices.length; ++i) {
+            this._detectionSpheres[i].isDetected = false;
+        }
+    }
+
+    private carPassedDetectionSphere (car: Car ): boolean {
+        for (let i: number = 0; i < this._detectionSpheres.length; i++) {
+            if (this._detectionSpheres[i].containsPoint(car.currentPosition)) {
+                if (this._detectionSpheres[i].isDetected === false) {
+                        this._detectionSpheres[i].isDetected = true;
+
+                        return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private createDetectionSpheres(): void {
+        for (let i: number = 0; i < this._trackVertices.length; ++i) {
+            this._detectionSpheres.push(new TrackingSphere(this._trackVertices[i]));
+            this._detectionSpheres[i].sphereIndex = i;
+        }
+    }
+
+    public updateTrackPortionIndex(car: Car): void {
+        if (this.carPassedDetectionSphere(car) === true) {
+            if (car.trackPortionIndex + 1 >= this._trackVertices.length) {
+                car.trackPortionIndex = 0;
+                this.resetDetectionSpheres();
+            } else {
+                car.trackPortionIndex++;
+            }
+        }
+    }
+
+}
