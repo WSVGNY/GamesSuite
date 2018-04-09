9ed3120f28f64524f382b2ad9e0e2532ff39a225
Anred <nissow301@gmail.com>
2018-04-09 16:48:25 -0400

    [CA-10] Creating another sphereDetection class

---
+++ Cadriciel/client/src/app/racing/carTracking-manager/sphereDetection.ts
@@ -0,0 +1,14 @@
+import { Vector3, Sphere } from "three";
+import { TRACKING_SPHERE_RADIUS } from "../constants";
+
+export class TrackingSphere extends Sphere {
+
+    public isDetected: boolean;
+    public sphereIndex: number;
+
+    public constructor(center: Vector3) {
+        super(center, TRACKING_SPHERE_RADIUS);
+        this.isDetected = false;
+        this.sphereIndex = 0;
+    }
+}
