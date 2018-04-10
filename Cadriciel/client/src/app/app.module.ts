d392f46c1c697ab0efc69270098f8876a3ef2587
charlesnollet <chrlsnllt@gmail.com>
2018-04-10 10:48:13 -0400

    [CA-11] Component creation

--- Cadriciel/client/src/app/app.module.ts
+++ Cadriciel/client/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { CameraManagerService } from "./racing/cameras/camera-manager.service";
 import { BestTimesComponent } from "./racing/scoreboard/best-times/best-times.component";
 import { InputTimeComponent } from "./racing/scoreboard/input-time/input-time.component";
 import { HighscoreService } from "./racing/scoreboard/highscore.service";
+import { EndGameTableComponent } from './racing/scoreboard/end-game-table/end-game-table.component';
 
 @NgModule({
     declarations: [
@@ -35,7 +36,8 @@ import { HighscoreService } from "./racing/scoreboard/highscore.service";
         ChooseTrackComponent,
         ConfigurationComponent,
         BestTimesComponent,
-        InputTimeComponent
+        InputTimeComponent,
+        EndGameTableComponent
     ],
     imports: [
         BrowserModule,
---
+++ Cadriciel/client/src/app/racing/scoreboard/end-game-table/end-game-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-end-game-table',
+  templateUrl: './end-game-table.component.html',
+  styleUrls: ['./end-game-table.component.css']
+})
+export class EndGameTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
