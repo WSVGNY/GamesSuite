76c5ddd03a57c6404ae5ee13921c043195ee67df
charlesnollet <chrlsnllt@gmail.com>
2018-04-10 09:29:59 -0400

    [CA-12] Added constants and div

--- Cadriciel/client/src/app/racing/scoreboard/best-times/best-times.component.ts
+++ Cadriciel/client/src/app/racing/scoreboard/best-times/best-times.component.ts
@@ -2,6 +2,9 @@ import { Component } from "@angular/core";
 import { HighscoreService } from "../highscore.service";
 import { CommonHighscore } from "../../../../../../common/racing/commonHighscore";
 
+const HUNDREDTH_TO_MINUTES: number = 6000;
+const SECONDS_TO_MINUTES: number = 100;
+
 @Component({
     selector: "app-best-times",
     templateUrl: "./best-times.component.html",
@@ -17,13 +20,13 @@ export class BestTimesComponent {
 
     public getTime(score: CommonHighscore): string {
         let time: string = "";
-        const minutes: number = Math.floor(score.time / 6000);
-        const seconds: number = Math.floor(Math.floor(score.time - minutes * 6000) / 100);
+        const minutes: number = Math.floor(score.time / HUNDREDTH_TO_MINUTES);
+        const seconds: number = Math.floor(Math.floor(score.time - minutes * HUNDREDTH_TO_MINUTES) / SECONDS_TO_MINUTES);
         time += minutes;
         time += ":";
         time += seconds;
         time += ":";
-        time += score.time - minutes * 6000 - seconds * 100;
+        time += score.time - minutes * HUNDREDTH_TO_MINUTES - seconds * SECONDS_TO_MINUTES;
 
         return time;
     }
