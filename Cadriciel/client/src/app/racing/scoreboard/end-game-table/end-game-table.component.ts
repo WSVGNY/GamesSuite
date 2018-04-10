bdf358fdcebcf5c71d5728528261bcb7b433647c
chbinc <chelsy.binet@polymtl.ca>
2018-04-10 10:52:22 -0400

    [CA-11] changement component au debut

--- Cadriciel/client/src/app/racing/scoreboard/end-game-table/end-game-table.component.ts
+++ Cadriciel/client/src/app/racing/scoreboard/end-game-table/end-game-table.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-end-game-table',
-  templateUrl: './end-game-table.component.html',
-  styleUrls: ['./end-game-table.component.css']
+  selector: "app-end-game-table",
+  templateUrl: "./end-game-table.component.html",
+  styleUrls: ["./end-game-table.component.css"]
 })
-export class EndGameTableComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class EndGameTableComponent {
 
 }
