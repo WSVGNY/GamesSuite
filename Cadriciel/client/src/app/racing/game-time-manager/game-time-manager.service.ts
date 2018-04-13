cf18c4f17229dd76655b358260a6f594700599a9
silegc <simon.legault@polymtl.ca>
2018-04-13 11:53:42 -0400

    [REFACTOR] Started game-time-manager refactor

--- Cadriciel/client/src/app/racing/game-time-manager/game-time-manager.service.ts
+++ Cadriciel/client/src/app/racing/game-time-manager/game-time-manager.service.ts
@@ -3,6 +3,25 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class GameTimeManagerService {
 
-  public constructor() { }
+    private _lastFrameDate: number;
+    private _raceStartDate: number;
+
+    public constructor() {
+        this._lastFrameDate = 0;
+        this._raceStartDate = 0;
+    }
+
+    public initializeTimes(): void {
+        this._lastFrameDate = Date.now();
+        this._raceStartDate = Date.now();
+    }
+
+    public get lastDate(): number {
+        return this._lastFrameDate;
+    }
+
+    public get startDate(): number {
+        return this._raceStartDate;
+    }
 
 }
