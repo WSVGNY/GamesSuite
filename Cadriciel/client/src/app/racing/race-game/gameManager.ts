2aeafe6b4b6ddde47b89c605f9065fa8eb3f5665
wisev <wsvgny@gmail.com>
2018-04-13 11:28:38 -0400

    [REFACTOR] Creted game-manager class

---
+++ Cadriciel/client/src/app/racing/race-game/gameManager.ts
@@ -0,0 +1,14 @@
+import { Track } from "../../../../../common/racing/track";
+import { Car } from "../car/car";
+import { Player } from "../../../../../common/crossword/player";
+
+export class GameManager {
+
+    private _chosenTrack: Track;
+    private _cars: Car[];
+    private _players: Player[];
+
+    public constructor() {
+
+    }
+}
