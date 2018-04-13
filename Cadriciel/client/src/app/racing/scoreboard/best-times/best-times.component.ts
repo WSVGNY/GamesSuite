8b072b4037efdd53e2be5b76bc621950f9a17875
chbinc <chelsy.binet@polymtl.ca>
2018-04-13 14:00:20 -0400

    [CA-11] formattage temps

--- Cadriciel/client/src/app/racing/scoreboard/best-times/best-times.component.ts
+++ Cadriciel/client/src/app/racing/scoreboard/best-times/best-times.component.ts
@@ -18,16 +18,17 @@ export class BestTimesComponent {
     }
 
     public getFormatedTime(score: number): string {
-        let time: string = "";
+        const time: string[] = ["00", "00", "00"];
         const minutes: number = Math.floor(score / SECONDS_TO_MINUTES);
         const seconds: number = Math.floor(score - minutes * SECONDS_TO_MINUTES);
         const hundredth: number = Math.round((score - minutes * SECONDS_TO_MINUTES - seconds) * SECONDS_TO_HUNDREDTH);
-        time += minutes;
-        time += ":";
-        time += seconds;
-        time += ":";
-        time += hundredth;
+        const minutesString: string = minutes.toString();
+        const secondsString: string = seconds.toString();
+        const hundredthString: string = hundredth.toString();
+        time[0] = (time[0] + minutesString).substring(minutesString.length);
+        time[1] = (time[1] + secondsString).substring(secondsString.length);
+        time[2] = (time[2] + hundredthString).substring(hundredthString.length);
 
-        return time;
+        return time.join(" : ");
     }
 }
