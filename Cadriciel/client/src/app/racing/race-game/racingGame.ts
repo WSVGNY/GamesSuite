81ec305e45fdd4bfdebcb67038622f5b69852343
wisev <wsvgny@gmail.com>
2018-04-15 15:39:10 -0400

    [RF-01] Implemented class attributes and two getters (getters currently not functionnal)

--- Cadriciel/client/src/app/racing/race-game/racingGame.ts
+++ Cadriciel/client/src/app/racing/race-game/racingGame.ts
@@ -1,3 +1,28 @@
+import { Player } from "./player";
+import { Car } from "../car/car";
+import { Track } from "../../../../../common/racing/track";
+import { GameScene } from "../scenes/gameScene";
+import { CameraManagerService } from "../cameras/camera-manager.service";
+import { Vector3, Camera } from "three";
+
 export class RacingGame {
 
+    private _players: Player[];
+    private _cars: Car[];
+    private _track: Track;
+    private _gameScene: GameScene;
+
+    public constructor(private _cameraManager: CameraManagerService) { }
+
+    public updateCamera(position: Vector3, elaspedTime?: number): void {
+        this._cameraManager.updateCameraPositions(position, elaspedTime);
+    }
+
+    public get playerCarPosition(): Vector3 {
+        return undefined;
+    }
+
+    public get currentCamera(): Camera {
+        return undefined;
+    }
 }
