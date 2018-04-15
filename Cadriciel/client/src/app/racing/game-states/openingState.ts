f5c38c40e6ba5ea28eb72c34ba46e9a0589e1890
wisev <wsvgny@gmail.com>
2018-04-15 15:37:28 -0400

    [RF-01] Started implementing update() in OpeningState

--- Cadriciel/client/src/app/racing/game-states/openingState.ts
+++ Cadriciel/client/src/app/racing/game-states/openingState.ts
@@ -1,12 +1,26 @@
 import { State } from "./state";
+import { AbstractState } from "./abstractState";
+import { RacingGame } from "../race-game/racingGame";
 
-export class OpeningState implements State {
+const MINIMUM_CAR_TO_CAMERA_DISTANCE: number = 3;
+export class OpeningState extends AbstractState implements State {
 
-    public constructor() {
-
+    public update(racingGame: RacingGame): void {
+        racingGame.updateCamera(racingGame.playerCarPosition, this.gameTimeManager.elaspedTime);
+        if (this.isAnimationOver(racingGame)) {
+            this.advanceToNextState(racingGame);
+        }
     }
 
-    public update(): void {
+    private isAnimationOver(racingGame: RacingGame): boolean {
+        return racingGame.currentCamera.position.clone().distanceTo(racingGame.playerCarPosition) < MINIMUM_CAR_TO_CAMERA_DISTANCE;
+    }
 
+    private advanceToNextState(racingGame: RacingGame): void {
+        this.gameTimeManager.resetStartDate();
+        // this._countDownOnScreenValue = "3";
+        this._currentState = State.COUNTDOWN;
+        this._cameraManager.changeToThirdPersonCamera();
+        this._soundManager.playCurrentStartSequenceSound();
     }
 }
