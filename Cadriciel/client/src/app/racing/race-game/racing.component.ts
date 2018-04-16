88f8a582297317c32e9b891c8ab118560e94aeed
wisev <wsvgny@gmail.com>
2018-04-16 15:04:50 -0400

    [RF-01] Commented time simulate functions that are migrated

--- Cadriciel/client/src/app/racing/race-game/racing.component.ts
+++ Cadriciel/client/src/app/racing/race-game/racing.component.ts
@@ -21,12 +21,9 @@ import { Player } from "./player";
 import { MINIMUM_CAR_DISTANCE, NUMBER_OF_LAPS } from "../constants/car.constants";
 import { AI_CARS_QUANTITY, AI_PERSONALITY_QUANTITY } from "../constants/ai.constants";
 import { CURRENT_PLAYER, COMPUTER_PLAYER } from "../constants/global.constants";
-<<<<<<< HEAD
 import { GameTimeManagerService } from "../game-time-manager/game-time-manager.service";
-=======
 import { HumanCar } from "../car/humanCar";
 import { AICar } from "../car/aiCar";
->>>>>>> Dev
 
 enum State {
     START_ANIMATION = 1,
@@ -185,55 +182,55 @@ export class RacingComponent implements AfterViewInit, OnInit {
         }
     }
 
-    private simulateRaceTime(currentSegmentIndex: number, segmentCounted: number, position: Vector3): number {
-        const lapSegmentAmount: number = this._chosenTrack.vertices.length;
-        const totalSegmentAmount: number = lapSegmentAmount * NUMBER_OF_LAPS;
-        const segmentsToGo: number = totalSegmentAmount - segmentCounted;
-        const completeLapsToGo: number = Math.floor(segmentsToGo / lapSegmentAmount);
+    // private simulateRaceTime(currentSegmentIndex: number, segmentCounted: number, position: Vector3): number {
+    //     const lapSegmentAmount: number = this._chosenTrack.vertices.length;
+    //     const totalSegmentAmount: number = lapSegmentAmount * NUMBER_OF_LAPS;
+    //     const segmentsToGo: number = totalSegmentAmount - segmentCounted;
+    //     const completeLapsToGo: number = Math.floor(segmentsToGo / lapSegmentAmount);
 
-        return this.simulateCompleteLapTime(completeLapsToGo) +
-            this.simulatePartialLapTime(currentSegmentIndex) +
-            this.simulatePartialSegmentTime(currentSegmentIndex, position);
+    //     return this.simulateCompleteLapTime(completeLapsToGo) +
+    //         this.simulatePartialLapTime(currentSegmentIndex) +
+    //         this.simulatePartialSegmentTime(currentSegmentIndex, position);
 
-    }
+    // }
 
-    private simulateCompleteLapTime(lapAmount: number): number {
-        let simulatedTime: number = 0;
-        for (let i: number = 0; i < this._chosenTrack.vertices.length; ++i) {
-            if ((i + 1) !== this._chosenTrack.vertices.length) {
-                const currentVertice: Vector3 = new Vector3(this._chosenTrack.vertices[i].x, 0, this._chosenTrack.vertices[i].z);
-                const nextVertice: Vector3 = new Vector3(this._chosenTrack.vertices[i + 1].x, 0, this._chosenTrack.vertices[i + 1].z);
-                simulatedTime += (currentVertice.distanceTo(nextVertice) / AVERAGE_CAR_SPEED);
-            }
-        }
+    // private simulateCompleteLapTime(lapAmount: number): number {
+    //     let simulatedTime: number = 0;
+    //     for (let i: number = 0; i < this._chosenTrack.vertices.length; ++i) {
+    //         if ((i + 1) !== this._chosenTrack.vertices.length) {
+    //             const currentVertice: Vector3 = new Vector3(this._chosenTrack.vertices[i].x, 0, this._chosenTrack.vertices[i].z);
+    //             const nextVertice: Vector3 = new Vector3(this._chosenTrack.vertices[i + 1].x, 0, this._chosenTrack.vertices[i + 1].z);
+    //             simulatedTime += (currentVertice.distanceTo(nextVertice) / AVERAGE_CAR_SPEED);
+    //         }
+    //     }
 
-        return simulatedTime * lapAmount;
-    }
+    //     return simulatedTime * lapAmount;
+    // }
 
-    private simulatePartialLapTime(currentSegmentIndex: number): number {
-        let simulatedTime: number = 0;
-        if (currentSegmentIndex !== 0) {
-            for (let i: number = currentSegmentIndex; i < this._chosenTrack.vertices.length; ++i) {
-                if ((i + 1) !== this._chosenTrack.vertices.length) {
-                    const currentVertice: Vector3 = new Vector3(this._chosenTrack.vertices[i].x, 0, this._chosenTrack.vertices[i].z);
-                    const nextVertice: Vector3 = new Vector3(this._chosenTrack.vertices[i + 1].x, 0, this._chosenTrack.vertices[i + 1].z);
-                    simulatedTime += (currentVertice.distanceTo(nextVertice) / AVERAGE_CAR_SPEED);
-                }
-            }
-        }
+    // private simulatePartialLapTime(currentSegmentIndex: number): number {
+    //     let simulatedTime: number = 0;
+    //     if (currentSegmentIndex !== 0) {
+    //         for (let i: number = currentSegmentIndex; i < this._chosenTrack.vertices.length; ++i) {
+    //             if ((i + 1) !== this._chosenTrack.vertices.length) {
+    //                 const currentVertice: Vector3 = new Vector3(this._chosenTrack.vertices[i].x, 0, this._chosenTrack.vertices[i].z);
+    //                 const nextVertice: Vector3 = new Vector3(this._chosenTrack.vertices[i + 1].x, 0, this._chosenTrack.vertices[i + 1].z);
+    //                 simulatedTime += (currentVertice.distanceTo(nextVertice) / AVERAGE_CAR_SPEED);
+    //             }
+    //         }
+    //     }
 
-        return simulatedTime;
-    }
+    //     return simulatedTime;
+    // }
 
-    private simulatePartialSegmentTime(currentSegmentIndex: number, position: Vector3): number {
-        const nextTrackVertex: Vector3 = new Vector3(
-            this._chosenTrack.vertices[currentSegmentIndex].x,
-            0,
-            this._chosenTrack.vertices[currentSegmentIndex].z
-        );
+    // private simulatePartialSegmentTime(currentSegmentIndex: number, position: Vector3): number {
+    //     const nextTrackVertex: Vector3 = new Vector3(
+    //         this._chosenTrack.vertices[currentSegmentIndex].x,
+    //         0,
+    //         this._chosenTrack.vertices[currentSegmentIndex].z
+    //     );
 
-        return (position.distanceTo(nextTrackVertex) / AVERAGE_CAR_SPEED);
-    }
+    //     return (position.distanceTo(nextTrackVertex) / AVERAGE_CAR_SPEED);
+    // }
 
     private updateEnd(): void {
         if (this._endGameTableService.players.length === 0) {
