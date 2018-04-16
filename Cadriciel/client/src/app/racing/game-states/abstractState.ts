fde86e260af2b1b9301fd3b54dcb30bbd0ab1084
wisev <wsvgny@gmail.com>
2018-04-15 22:16:05 -0400

    [RF-01] Implemented common constructor in abstractState

--- Cadriciel/client/src/app/racing/game-states/abstractState.ts
+++ Cadriciel/client/src/app/racing/game-states/abstractState.ts
@@ -1,10 +1,16 @@
-import { GameUpdateManagerService } from "../game-update-manager/game-update-manager.service";
+import { CollisionManagerService } from "../collision-manager/collision-manager.service";
+import { CameraManagerService } from "../cameras/camera-manager.service";
+import { CarTrackingManagerService } from "../carTracking-manager/car-tracking-manager.service";
 import { GameTimeManagerService } from "../game-time-manager/game-time-manager.service";
+import { AICarService } from "../artificial-intelligence/ai-car.service";
 
 export abstract class AbstractState {
 
     public constructor(
-        protected gameUpdateManager: GameUpdateManagerService,
-        protected gameTimeManager: GameTimeManagerService
+        protected _aiCarService: AICarService,
+        protected _collisionManager: CollisionManagerService,
+        protected _cameraManager: CameraManagerService,
+        protected _trackingManager: CarTrackingManagerService,
+        protected _gameTimeManager: GameTimeManagerService
     ) { }
 }
