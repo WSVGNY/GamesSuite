5b0ba09c0b1252ac4ca6d6d92d5960813157b335
wisev <wsvgny@gmail.com>
2018-04-16 15:04:27 -0400

    [RF-01] Implemented resultsState update method

--- Cadriciel/client/src/app/racing/game-states/resultsState.ts
+++ Cadriciel/client/src/app/racing/game-states/resultsState.ts
@@ -1,12 +1,27 @@
 import { State } from "./state";
+import { AbstractState } from "./abstractState";
+import { RacingGame } from "../race-game/racingGame";
+import { GameUpdateManagerService } from "../game-update-manager/game-update-manager.service";
 
-export class ResultsState implements State {
+export class ResultsState extends AbstractState implements State {
 
-    public constructor() {
-
+    public update(gameUpdateManager: GameUpdateManagerService, racingGame: RacingGame): void {
+        for (const car of racingGame.cars) {
+            if (!car.raceProgressTracker.isRaceCompleted && !car.raceProgressTracker.isTimeLogged) {
+                racingGame.getPlayerById(car.uniqueid).setTotalTime(
+                    this._gameTimeManager.elaspedTime +
+                    this._gameTimeManager.simulateRaceTime(
+                        car.raceProgressTracker,
+                        car.currentPosition,
+                        racingGame.track
+                    )
+                );
+                car.raceProgressTracker.isTimeLogged = true;
+            }
+        }
     }
 
-    public update(): void {
+    public advanceToNextState(gameUpdateManager: GameUpdateManagerService): void {
 
     }
 }
