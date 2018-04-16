083fc4c3786c4f6ab1d0b0bdd12edaf7e84854bc
wisev <wsvgny@gmail.com>
2018-04-16 16:10:40 -0400

    [RF-01] Implemented and reworked time-manager simulate methods

--- Cadriciel/client/src/app/racing/game-time-manager/game-time-manager.service.ts
+++ Cadriciel/client/src/app/racing/game-time-manager/game-time-manager.service.ts
@@ -2,65 +2,56 @@ import { Injectable } from "@angular/core";
 import { Vector3 } from "three";
 import { RaceProgressTracker } from "../carTracking-manager/raceProgressTracker";
 import { Track } from "../../../../../common/racing/track";
+import { NUMBER_OF_LAPS } from "../constants/car.constants";
+
+const AVERAGE_CAR_SPEED: number = 45;
 
 @Injectable()
 export class GameTimeManagerService {
 
-    private _lastFrameDate: number;
-    private _raceStartDate: number;
-
     private _startDate: number;
-    private _elaspedTime: number;
+    private _lastDate: number;
 
     public constructor() {
-        this._lastFrameDate = 0;
-        this._raceStartDate = 0;
+        this.initializeDates();
     }
 
-    public initializeTimes(): void {
-        this._lastFrameDate = Date.now();
-        this._raceStartDate = Date.now();
+    private initializeDates(): void {
+        this._startDate = 0;
+        this._lastDate = 0;
     }
 
-    public get lastDate(): number {
-        return this._lastFrameDate;
-    }
-
-    public get startDate(): number {
-        return this._raceStartDate;
-    }
-
-    public get elaspedTime(): number {
-        return this._elaspedTime;
+    public getElaspedTime(): number {
+        return Date.now() - this._startDate;
     }
 
     public getTimeSinceLastFrame(): number {
-        return Date.now() - this._lastFrameDate;
+        return Date.now() - this._lastDate;
     }
 
     public resetStartDate(): void {
         this._startDate = Date.now();
-        this._elaspedTime = 0;
     }
 
-    public simulateRaceTime(raceProgressTracker: RaceProgressTracker, position: Vector3, track: Track): number {
-        const lapSegmentAmount: number = this._chosenTrack.vertices.length;
-        const totalSegmentAmount: number = lapSegmentAmount * NUMBER_OF_LAPS;
-        const segmentsToGo: number = totalSegmentAmount - segmentCounted;
-        const completeLapsToGo: number = Math.floor(segmentsToGo / lapSegmentAmount);
+    public simulateRaceTime(raceProgressTracker: RaceProgressTracker, currentPosition: Vector3, track: Track): number {
+        const lapSegmentCount: number = track.vertices.length;
+        const raceSegmentCount: number = lapSegmentCount * NUMBER_OF_LAPS;
+        const remainingSegmentCount: number = raceSegmentCount - raceProgressTracker.segmentCounted;
+        const remainingLapCount: number = Math.floor(remainingSegmentCount / lapSegmentCount);
 
-        return this.simulateCompleteLapTime(completeLapsToGo) +
-            this.simulatePartialLapTime(currentSegmentIndex) +
-            this.simulatePartialSegmentTime(currentSegmentIndex, position);
+        return this.getElaspedTime() +
+            this.simulateCompleteLapTime(track, remainingLapCount) +
+            this.simulatePartialLapTime(track, raceProgressTracker.currentSegmentIndex) +
+            this.simulatePartialSegmentTime(currentPosition, track, raceProgressTracker.currentSegmentIndex);
 
     }
 
-    private simulateCompleteLapTime(lapAmount: number): number {
+    private simulateCompleteLapTime(track: Track, lapAmount: number): number {
         let simulatedTime: number = 0;
-        for (let i: number = 0; i < this._chosenTrack.vertices.length; ++i) {
-            if ((i + 1) !== this._chosenTrack.vertices.length) {
-                const currentVertice: Vector3 = new Vector3(this._chosenTrack.vertices[i].x, 0, this._chosenTrack.vertices[i].z);
-                const nextVertice: Vector3 = new Vector3(this._chosenTrack.vertices[i + 1].x, 0, this._chosenTrack.vertices[i + 1].z);
+        for (let i: number = 0; i < track.vertices.length; ++i) {
+            if ((i + 1) !== track.vertices.length) {
+                const currentVertice: Vector3 = new Vector3(track.vertices[i].x, 0, track.vertices[i].z);
+                const nextVertice: Vector3 = new Vector3(track.vertices[i + 1].x, 0, track.vertices[i + 1].z);
                 simulatedTime += (currentVertice.distanceTo(nextVertice) / AVERAGE_CAR_SPEED);
             }
         }
@@ -68,13 +59,13 @@ export class GameTimeManagerService {
         return simulatedTime * lapAmount;
     }
 
-    private simulatePartialLapTime(currentSegmentIndex: number): number {
+    private simulatePartialLapTime(track: Track, currentSegmentIndex: number): number {
         let simulatedTime: number = 0;
         if (currentSegmentIndex !== 0) {
-            for (let i: number = currentSegmentIndex; i < this._chosenTrack.vertices.length; ++i) {
-                if ((i + 1) !== this._chosenTrack.vertices.length) {
-                    const currentVertice: Vector3 = new Vector3(this._chosenTrack.vertices[i].x, 0, this._chosenTrack.vertices[i].z);
-                    const nextVertice: Vector3 = new Vector3(this._chosenTrack.vertices[i + 1].x, 0, this._chosenTrack.vertices[i + 1].z);
+            for (let i: number = currentSegmentIndex; i < track.vertices.length; ++i) {
+                if ((i + 1) !== track.vertices.length) {
+                    const currentVertice: Vector3 = new Vector3(track.vertices[i].x, 0, track.vertices[i].z);
+                    const nextVertice: Vector3 = new Vector3(track.vertices[i + 1].x, 0, track.vertices[i + 1].z);
                     simulatedTime += (currentVertice.distanceTo(nextVertice) / AVERAGE_CAR_SPEED);
                 }
             }
@@ -83,11 +74,11 @@ export class GameTimeManagerService {
         return simulatedTime;
     }
 
-    private simulatePartialSegmentTime(currentSegmentIndex: number, position: Vector3): number {
+    private simulatePartialSegmentTime(position: Vector3, track: Track, currentSegmentIndex: number): number {
         const nextTrackVertex: Vector3 = new Vector3(
-            this._chosenTrack.vertices[currentSegmentIndex].x,
+            track.vertices[currentSegmentIndex].x,
             0,
-            this._chosenTrack.vertices[currentSegmentIndex].z
+            track.vertices[currentSegmentIndex].z
         );
 
         return (position.distanceTo(nextTrackVertex) / AVERAGE_CAR_SPEED);
