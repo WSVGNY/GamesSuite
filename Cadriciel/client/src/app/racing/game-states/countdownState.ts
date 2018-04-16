4ac019879914064e7049a39312be93749eef3a86
wisev <wsvgny@gmail.com>
2018-04-15 22:41:21 -0400

    [RF-01] Implemented countdownState update function

--- Cadriciel/client/src/app/racing/game-states/countdownState.ts
+++ Cadriciel/client/src/app/racing/game-states/countdownState.ts
@@ -1,12 +1,27 @@
 import { State } from "./state";
+import { AbstractState } from "./abstractState";
+import { GameUpdateManagerService } from "../game-update-manager/game-update-manager.service";
+import { RacingGame } from "../race-game/racingGame";
 
-export class CountdowngState implements State {
+export class CountdowngState extends AbstractState implements State {
 
-    public constructor() {
-
+    public update(gameUpdateManager: GameUpdateManagerService, racingGame: RacingGame): void {
+        let countdownValue: number = +racingGame.countdownOnScreenValue;
+        racingGame.countdownOnScreenValue = (--countdownValue).toString();
+        if (this.isCountDownOver(countdownValue)) {
+            racingGame.isCountdownOver = true;
+            racingGame.countdownOnScreenValue = "START";
+            this.advanceToNextState(gameUpdateManager);
+        }
     }
 
-    public update(): void {
+    private isCountDownOver(countdownValue: number): boolean {
+        return countdownValue === 0;
+    }
 
+    private advanceToNextState(gameUpdateManager: GameUpdateManagerService): void {
+        gameUpdateManager.setState("RACING");
+        this._gameTimeManager.resetStartDate();
+        // this._startDate = Date.now();
     }
 }
