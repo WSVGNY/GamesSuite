74c9deb6750b8d03c5d2ebf7a8c8fa6db37b7973
wisev <wsvgny@gmail.com>
2018-04-15 22:17:07 -0400

    [RF-01] Implemented stateFactory and init function

--- Cadriciel/client/src/app/racing/game-update-manager/game-update-manager.service.ts
+++ Cadriciel/client/src/app/racing/game-update-manager/game-update-manager.service.ts
@@ -1,32 +1,46 @@
 import { Injectable } from "@angular/core";
-import { Track } from "../../../../../common/racing/track";
 import { CarTrackingManagerService } from "../carTracking-manager/car-tracking-manager.service";
 import { CollisionManagerService } from "../collision-manager/collision-manager.service";
 import { GameTimeManagerService } from "../game-time-manager/game-time-manager.service";
 import { AICarService } from "../artificial-intelligence/ai-car.service";
 import { State } from "../game-states/state";
-import { OpeningState } from "../game-states/openingState";
 import { RacingGame } from "../race-game/racingGame";
-import { Vector3 } from "three";
+import { StateFactory } from "../game-states/stateFactory";
+import { CameraManagerService } from "../cameras/camera-manager.service";
 
 @Injectable()
 export class GameUpdateManagerService {
 
+  private _stateFactory: StateFactory;
   private _currentState: State;
 
   public constructor(
-    private _trackingManager: CarTrackingManagerService,
+    private _aiCarService: AICarService,
     private _collisionManager: CollisionManagerService,
-    private _timeManager: GameTimeManagerService,
-    private _aiCarService: AICarService
-  ) { }
+    private _cameraManager: CameraManagerService,
+    private _trackingManager: CarTrackingManagerService,
+    private _gameTimeManager: GameTimeManagerService
+  ) {
+    this.initialize();
+  }
 
   private initialize(): void {
-    this._currentState = new OpeningState(this, this._timeManager);
+    this._stateFactory = new StateFactory(
+      this._aiCarService,
+      this._collisionManager,
+      this._cameraManager,
+      this._trackingManager,
+      this._gameTimeManager
+    );
+    this.setState("OPENING");
+  }
+
+  public setState(stateType: string): void {
+    this._currentState = this._stateFactory.getState(stateType);
   }
 
   public update(racingGame: RacingGame): void {
-    this._currentState.update(racingGame);
+    this._currentState.update(this, racingGame);
   }
 
 }
