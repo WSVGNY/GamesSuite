6c8ad47613e28092850f207e3a976031c0459300
wisev <wsvgny@gmail.com>
2018-04-15 22:58:49 -0400

    [RF-01] Removed references to the update services from the update-manager, They're now only in the states

--- Cadriciel/client/src/app/racing/game-update-manager/game-update-manager.service.ts
+++ Cadriciel/client/src/app/racing/game-update-manager/game-update-manager.service.ts
@@ -1,37 +1,20 @@
 import { Injectable } from "@angular/core";
-import { CarTrackingManagerService } from "../carTracking-manager/car-tracking-manager.service";
-import { CollisionManagerService } from "../collision-manager/collision-manager.service";
-import { GameTimeManagerService } from "../game-time-manager/game-time-manager.service";
-import { AICarService } from "../artificial-intelligence/ai-car.service";
 import { State } from "../game-states/state";
 import { RacingGame } from "../race-game/racingGame";
-import { StateFactory } from "../game-states/stateFactory";
-import { CameraManagerService } from "../cameras/camera-manager.service";
+import { StateFactoryService } from "../game-states/state-factory/state-factory.service";
 
 @Injectable()
 export class GameUpdateManagerService {
 
-  private _stateFactory: StateFactory;
   private _currentState: State;
 
   public constructor(
-    private _aiCarService: AICarService,
-    private _collisionManager: CollisionManagerService,
-    private _cameraManager: CameraManagerService,
-    private _trackingManager: CarTrackingManagerService,
-    private _gameTimeManager: GameTimeManagerService
+    private _stateFactory: StateFactoryService
   ) {
     this.initialize();
   }
 
   private initialize(): void {
-    this._stateFactory = new StateFactory(
-      this._aiCarService,
-      this._collisionManager,
-      this._cameraManager,
-      this._trackingManager,
-      this._gameTimeManager
-    );
     this.setState("OPENING");
   }
 
