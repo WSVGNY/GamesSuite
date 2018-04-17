67191a4ceb08eceff95e62e95d9380eeb0f93720
Anred <nissow301@gmail.com>
2018-04-16 20:09:35 -0400

    [Tests]CarTrackingManager testing f

--- Cadriciel/client/src/app/racing/carTracking-manager/car-tracking-manager.service.spec.ts
+++ Cadriciel/client/src/app/racing/carTracking-manager/car-tracking-manager.service.spec.ts
@@ -7,6 +7,12 @@ import { Vector3, Sphere } from "three";
 describe("CarTrackingManagerService", () => {
     const raceProgressTracker: RaceProgressTracker = new RaceProgressTracker();
     const trackingManager: CarTrackingManagerService = new CarTrackingManagerService();
+    const MOCK_TRACK: Vector3[] = [
+        new Vector3(0, 0, 0),
+        new Vector3(100, 0, 0),
+        new Vector3(100, 0, 100),
+        new Vector3(0, 0, 100),
+    ];
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [CarTrackingManagerService]
@@ -18,22 +24,18 @@ describe("CarTrackingManagerService", () => {
     }));
 
     it("should be at finish line", () => {
-        expect(trackingManager["isAtFinishLine"](new Vector3(0, 0, 0), raceProgressTracker)).toEqual(true);
+        trackingManager.init(MOCK_TRACK);
+        expect(trackingManager["isAtFinishLine"](new Vector3(50, 0, 1), raceProgressTracker)).toEqual(true);
     });
 
     it("shouldn't be at finish line", () => {
+        trackingManager.init(MOCK_TRACK);
         expect(trackingManager["isAtFinishLine"](new Vector3(10, 10, 10), raceProgressTracker)).toEqual(false);
     });
 
-    it("should compute finish line", () => {
-        const MOCK_TRACK: Vector3[] = [
-            new Vector3(0, 0, 0),
-            new Vector3(100, 0, 0),
-            new Vector3(100, 0, 100),
-            new Vector3(0, 0, 100),
-        ];
+    it("should compute finish line correctly ", () => {
         trackingManager["computeFinishLine"](MOCK_TRACK);
-        expect(trackingManager["_finishLineSegment"].getPositionFromMatrix).toEqual(new Vector3(0, 0, 0));
+        expect(trackingManager["_finishLineSegment"]).toEqual(new Vector3(1, 0, 0));
     });
 
     it("should contains the car", () => {
@@ -44,5 +46,15 @@ describe("CarTrackingManagerService", () => {
     it("shouldn't contains the car", () => {
         const trackingSphere: Sphere = new Sphere(new Vector3(1, 0, 0));
         expect(trackingManager["sphereContainsCar"](trackingSphere, new Vector3(10, 0, 30))).toEqual(false);
+    });
+
+    it("should update the race correctly", () => {
+        // trackingManager.update(new Vector3(0, 0, 0), raceProgressTracker);
+        expect(trackingManager.update(new Vector3(0, 0, 0), raceProgressTracker)).toEqual(false);
+    });
+
+    it("should update the race correctly", () => {
+        trackingManager.update(new Vector3(0, 0, 0), raceProgressTracker);
+        expect(raceProgressTracker.isRaceCompleted).toEqual(false);
     });
 });
