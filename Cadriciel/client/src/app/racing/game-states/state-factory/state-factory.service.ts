8bfb3c09a93b2df8a932eeb64d8308ee6fd32cf3
silegc <simon.legault@polymtl.ca>
2018-04-17 09:33:34 -0400

    [RF-01] switch for getState

--- Cadriciel/client/src/app/racing/game-states/state-factory/state-factory.service.ts
+++ Cadriciel/client/src/app/racing/game-states/state-factory/state-factory.service.ts
@@ -11,70 +11,67 @@ import { GameUpdateManagerService } from "../../game-update-manager/game-update-
 @Injectable()
 export class StateFactoryService {
 
-  public constructor(
-    private _gameUpdateService: GameUpdateManagerService,
-  ) { }
+    public constructor(
+        private _gameUpdateService: GameUpdateManagerService,
+    ) { }
 
-  public getState(state: StateTypes): State {
-    if (state === undefined) {
-      return undefined;
+    public getState(state: StateTypes): State {
+        switch (state) {
+            case StateTypes.Opening:
+                return this.createOpeningState();
+            case StateTypes.Countdown:
+                return this.createCountdownState();
+            case StateTypes.Racing:
+                return this.createRacingState();
+            case StateTypes.Results:
+                return this.createResultsState();
+            case StateTypes.Closing:
+                return this.createClosingState();
+            default:
+                return undefined;
+        }
     }
 
-    if (state === StateTypes.Opening) {
-      return this.createOpeningState();
-    } else if (state === StateTypes.Countdown) {
-      return this.createCountdownState();
-    } else if (state === StateTypes.Racing) {
-      return this.createRacingState();
-    } else if (state === StateTypes.Results) {
-      return this.createResultsState();
-    } else if (state === StateTypes.Closing) {
-      return this.createClosingState();
+    private createOpeningState(): State {
+        return new OpeningState(
+            this._gameUpdateService.cameraService,
+            this._gameUpdateService.gameTimeService,
+            this._gameUpdateService.soundService
+        );
     }
 
-    return undefined;
-  }
+    private createCountdownState(): State {
+        return new CountdownState(
+            this._gameUpdateService.gameTimeService,
+            this._gameUpdateService.countdownService,
+            this._gameUpdateService.soundService
+        );
+    }
 
-  private createOpeningState(): State {
-    return new OpeningState(
-      this._gameUpdateService.cameraService,
-      this._gameUpdateService.gameTimeService,
-      this._gameUpdateService.soundService
-    );
-  }
+    private createRacingState(): State {
+        return new RacingState(
+            this._gameUpdateService.aiCarService,
+            this._gameUpdateService.collisionService,
+            this._gameUpdateService.cameraService,
+            this._gameUpdateService.trackingService,
+            this._gameUpdateService.gameTimeService,
+            this._gameUpdateService.soundService
+        );
+    }
 
-  private createCountdownState(): State {
-    return new CountdownState(
-      this._gameUpdateService.gameTimeService,
-      this._gameUpdateService.countdownService,
-      this._gameUpdateService.soundService
-    );
-  }
+    private createResultsState(): State {
+        return new ResultsState(
+            this._gameUpdateService.gameTimeService,
+            // this._soundManager
+        );
+    }
 
-  private createRacingState(): State {
-    return new RacingState(
-      this._gameUpdateService.aiCarService,
-      this._gameUpdateService.collisionService,
-      this._gameUpdateService.cameraService,
-      this._gameUpdateService.trackingService,
-      this._gameUpdateService.gameTimeService,
-      this._gameUpdateService.soundService
-    );
-  }
-
-  private createResultsState(): State {
-    return new ResultsState(
-      this._gameUpdateService.gameTimeService,
-      // this._soundManager
-    );
-  }
-
-  private createClosingState(): State {
-    return new ClosingState(
-      // this._gameUpdateService.aiCarService,
-      // this._gameUpdateService.gameTimeService,
-      // this._gameUpdateService.soundService
-    );
-  }
+    private createClosingState(): State {
+        return new ClosingState(
+            // this._gameUpdateService.aiCarService,
+            // this._gameUpdateService.gameTimeService,
+            // this._gameUpdateService.soundService
+        );
+    }
 
 }
