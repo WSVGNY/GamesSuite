70d2558ec62d3398fd7083146525cfa8de2c9c29
Philippe Kavalec <philippe_kavalec@hotmail.com>
2018-04-17 19:09:22 -0400

    [hotfix] forgot to stage changes

--- Cadriciel/client/src/app/racing/collision-manager/carCollision-manager.spec.ts
+++ Cadriciel/client/src/app/racing/collision-manager/carCollision-manager.spec.ts
@@ -55,131 +55,84 @@ describe("Car collision Manager Service", () => {
 
     it("should detect if cars are colliding in an array of cars that are colliding", inject(
         [SoundManagerService], (_soundManager: SoundManagerService) => {
-            const cars: AICar[] = [];
-            cars.push(firstCar);
-            cars.push(secondCar);
-            cars[0].currentPosition.set(1, 0, 0);
+            cars[0].setCurrentPosition(new Vector3(1, 0, 0));
+            cars[0]["_mesh"].updateMatrix();
+            cars[0]["_hitbox"].updatePosition(cars[0].currentPosition, cars[0]["_mesh"].matrix);
             CarCollisionManager.update(cars, _soundManager);
-            expect(CarCollisionManager["computeCollisionParameters"]()).toEqual(true);
+            expect(cars[0].hitbox.inCollision).toEqual(true);
         }));
 
     it("should not detect if cars are colliding in an array of cars that aren't colliding", inject(
         [SoundManagerService], (_soundManager: SoundManagerService) => {
-            const cars: AICar[] = [];
-            cars.push(firstCar);
-            cars.push(secondCar);
-            cars[0].currentPosition.set(150, 0, MINIMUM_CAR_DISTANCE - 0.01);
+            cars[0].setCurrentPosition(new Vector3(0, 0, MINIMUM_CAR_DISTANCE));
+            cars[0]["_mesh"].updateMatrix();
+            cars[0]["_hitbox"].updatePosition(cars[0].currentPosition, cars[0]["_mesh"].matrix);
             CarCollisionManager.update(cars, _soundManager);
-            console.log(cars[0].currentPosition);
-            console.log(cars[1].currentPosition);
-            console.log(CarCollisionManager["checkIfCarsAreClose"]());
-            console.log(CarCollisionManager["computeCollisionParameters"]());
-            expect(CarCollisionManager["computeCollisionParameters"]()).toEqual(false);
+            expect(cars[0].hitbox.inCollision).toEqual(false);
         }));
 
-    it("collision point should be a vertex of the collision emitter's hitbox", () => {
-        // firstCar["_mesh"].position.set(1, 0, 1);
-        // firstCar["_mesh"].updateMatrix();
-        // const collidingVertex: Vector3 = firstCar.hitbox.bottomPlaneVertices[0].clone().applyMatrix4(firstCar.meshMatrix);
-        // const collisionPoint: Vector3 = CarCollisionManager["checkIfColliding"](firstCar, secondCar);
-        // expect(collidingVertex).toEqual(collisionPoint);
-        expect(false).toBe(true);
-    });
+    it("should resolve the hitbox overlap", inject(
+        [SoundManagerService], (_soundManager: SoundManagerService) => {
+            cars[0].setCurrentPosition(new Vector3(0, 0, 2.75));
+            cars[0]["_mesh"].updateMatrix();
+            cars[0].hitbox.updatePosition(new Vector3(0, 0, 2.75), cars[0]["_mesh"].matrix);
+            CarCollisionManager.update(cars, _soundManager);
+            expect(cars[0].currentPosition).not.toEqual(new Vector3(0, 0, 2.75));
+        }));
 
-    it("should resolve the hitbox overlap", () => {
-        // firstCar["_mesh"].position.set(1, 0, 1);
-        // firstCar["_mesh"].updateMatrix();
-        // CarCollisionManager["_collisionEmitter"] = firstCar;
-        // CarCollisionManager["_collisionReceiver"] = secondCar;
-        // CarCollisionManager["_collisionPoint"] = firstCar.hitbox.bottomPlaneVertices[0].clone().applyMatrix4(firstCar.meshMatrix);
-        // CarCollisionManager["resolveHitboxOverlap"]();
-        // CarCollisionManager["_collisionEmitter"]["_mesh"].updateMatrix();
-        // CarCollisionManager["_collisionReceiver"]["_mesh"].updateMatrix();
-        // const collidingVertex: Vector3 = CarCollisionManager["_collisionReceiver"].hitbox.bottomPlaneVertices[0]
-        //     .clone().applyMatrix4(firstCar.meshMatrix);
-        // const direction: Vector3 = collidingVertex.sub(CarCollisionManager["_collisionEmitter"].currentPosition);
-        // const ray: Raycaster = new Raycaster(
-        //     CarCollisionManager["_collisionEmitter"].currentPosition.clone(), direction.clone().normalize());
-        // const collisionResult: Intersection[] = ray.intersectObject(CarCollisionManager["_collisionReceiver"].hitbox);
-        // expect(collisionResult.length > 0 && collisionResult[0].distance < direction.length()).toEqual(false);
-        expect(false).toBe(true);
-    });
+    it("should change speed of first car", inject(
+        [SoundManagerService], (_soundManager: SoundManagerService) => {
+            cars[0].setCurrentPosition(new Vector3(0, 0, 2.75));
+            cars[0]["_carControls"].speed.add(new Vector3(0, 0, 5));
+            cars[0]["_mesh"].updateMatrix();
+            cars[0].hitbox.updatePosition(new Vector3(0, 0, 2.75), cars[0]["_mesh"].matrix);
 
-    it("should create a resulting force in the right direction on the collision emitter", () => {
-        // firstCar["_mesh"].position.set(-2, 0, 1);
-        // firstCar["_mesh"].updateMatrix();
-        // CarCollisionManager["_collisionEmitter"] = firstCar;
-        // CarCollisionManager["_collisionReceiver"] = secondCar;
-        // CarCollisionManager["_collisionPoint"] = firstCar.hitbox.bottomPlaneVertices[0].clone().applyMatrix4(firstCar.meshMatrix);
-        // firstCar.speed = new Vector3(0, 0, -5);
-        // secondCar.speed = new Vector3(0, 0, 0);
-        // const resultingForces: Vector3[] = CarCollisionManager["computeResultingForces"](
-        //     CarCollisionManager["_collisionEmitter"],
-        //     CarCollisionManager["_collisionReceiver"],
-        //     CarCollisionManager["_collisionPoint"]
-        // );
-        // expect(resultingForces[0].x).toBeLessThan(0);
-        // expect(resultingForces[0].z).toBeLessThan(0);
-        expect(false).toBe(true);
-    });
+            CarCollisionManager.update(cars, _soundManager);
+            expect(cars[0].speed).not.toEqual(new Vector3(0, 0, 5));
+        }));
 
-    it("should create a resulting force in the right direction on the collision receiver", () => {
-        // firstCar["_mesh"].position.set(-2, 0, 1);
-        // firstCar["_mesh"].updateMatrix();
-        // CarCollisionManager["_collisionEmitter"] = firstCar;
-        // CarCollisionManager["_collisionReceiver"] = secondCar;
-        // CarCollisionManager["_collisionPoint"] = firstCar.hitbox.bottomPlaneVertices[0].clone().applyMatrix4(firstCar.meshMatrix);
-        // firstCar.speed = new Vector3(0, 0, -5);
-        // secondCar.speed = new Vector3(0, 0, 0);
-        // const resultingForces: Vector3[] = CarCollisionManager["computeResultingForces"](
-        //     CarCollisionManager["_collisionEmitter"],
-        //     CarCollisionManager["_collisionReceiver"],
-        //     CarCollisionManager["_collisionPoint"]
-        // );
-        // expect(resultingForces[1].x).toBeLessThan(0);
-        // expect(resultingForces[1].z).toBeGreaterThan(0);
-        expect(false).toBe(true);
-    });
+    it("should change speed of second car", inject(
+        [SoundManagerService], (_soundManager: SoundManagerService) => {
+            cars[0].setCurrentPosition(new Vector3(0, 0, 2.75));
+            cars[0]["_carControls"].speed.add(new Vector3(0, 0, 5));
+            cars[0]["_mesh"].updateMatrix();
+            cars[0].hitbox.updatePosition(new Vector3(0, 0, 2.75), cars[0]["_mesh"].matrix);
 
-    it("collision should be elastic (no energy loss)", () => {
-        // firstCar["_mesh"].position.set(-2, 0, 1);
-        // firstCar["_mesh"].updateMatrix();
-        // CarCollisionManager["_collisionEmitter"] = firstCar;
-        // CarCollisionManager["_collisionReceiver"] = secondCar;
-        // CarCollisionManager["_collisionPoint"] = firstCar.hitbox.bottomPlaneVertices[0].clone().applyMatrix4(firstCar.meshMatrix);
-        // firstCar.speed = new Vector3(0, 0, -5);
-        // secondCar.speed = new Vector3(0, 0, 0);
-        // const resultingForces: Vector3[] = CarCollisionManager["computeResultingForces"](
-        //     CarCollisionManager["_collisionEmitter"],
-        //     CarCollisionManager["_collisionReceiver"],
-        //     CarCollisionManager["_collisionPoint"]
-        // );
-        // const forcesBeforeImpact: number = firstCar.speed.length();
-        // const forcesAfterImpact: number = (resultingForces[0].clone().add(resultingForces[1])).length();
-        // expect(Math.abs(forcesBeforeImpact - forcesAfterImpact)).toBeLessThan(0.0001);
-        expect(false).toBe(true);
-    });
+            CarCollisionManager.update(cars, _soundManager);
+            expect(cars[1].speed).not.toEqual(new Vector3(0, 0, 0));
+        }));
+
+    it("collision should be elastic (no energy loss)", inject(
+        [SoundManagerService], (_soundManager: SoundManagerService) => {
+            cars[0].setCurrentPosition(new Vector3(0, 0, 2.75));
+            cars[0]["_carControls"].speed.add(new Vector3(0, 0, 5));
+            cars[0]["_mesh"].updateMatrix();
+            cars[0].hitbox.updatePosition(new Vector3(0, 0, 2.75), cars[0]["_mesh"].matrix);
+
+            CarCollisionManager.update(cars, _soundManager);
+            expect(Math.abs(cars[1].speed.length() + cars[0].speed.length())).toEqual(5);
+        }));
 
     it("should push car in the direction it is facing", () => {
-        const force: Vector3 = new Vector3(1, 0, -1);
+        const force: Vector3 = new Vector3(0, 0, -1);
         const zComponent: number = CarCollisionManager["computeSpeedZComponent"](force, firstCar.direction);
         expect(zComponent).toBeGreaterThanOrEqual(0);
     });
 
     it("should push car in the opposite direction it is facing", () => {
-        const force: Vector3 = new Vector3(-1, 0, 1);
+        const force: Vector3 = new Vector3(0, 0, 1);
         const zComponent: number = CarCollisionManager["computeSpeedZComponent"](force, firstCar.direction);
         expect(zComponent).toBeLessThanOrEqual(0);
     });
 
     it("should push car to its left", () => {
-        const force: Vector3 = new Vector3(1, 0, -1);
+        const force: Vector3 = new Vector3(-1, 0, 0);
         const xComponent: number = CarCollisionManager["computeSpeedXComponent"](force, firstCar.direction);
         expect(xComponent).toBeGreaterThanOrEqual(0);
     });
 
     it("should push car to its right", () => {
-        const force: Vector3 = new Vector3(-1, 0, 1);
+        const force: Vector3 = new Vector3(1, 0, 0);
         const xComponent: number = CarCollisionManager["computeSpeedXComponent"](force, firstCar.direction);
         expect(xComponent).toBeLessThanOrEqual(0);
     });
