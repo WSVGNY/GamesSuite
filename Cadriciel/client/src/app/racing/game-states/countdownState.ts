7f38412ebfc75b87c8096f74bdb7e148c3bb42f3
wisev <wsvgny@gmail.com>
2018-04-17 03:07:39 -0400

    [RF-01] Added sounds to the countDownState

--- Cadriciel/client/src/app/racing/game-states/countdownState.ts
+++ Cadriciel/client/src/app/racing/game-states/countdownState.ts
@@ -3,7 +3,7 @@ import { RacingGame } from "../race-game/racingGame";
 import { StateTypes } from "./stateTypes";
 import { GameTimeManagerService } from "../game-time-manager/game-time-manager.service";
 import { CountdownService } from "../countdown/countdown.service";
-// import { SoundManagerService } from "../sound-service/sound-manager.service";
+import { SoundManagerService } from "../sound-service/sound-manager.service";
 
 const ONE_SECOND: number = 1000;
 
@@ -11,19 +11,21 @@ export class CountdownState implements State {
 
     public constructor(
         private _gameTimeManager: GameTimeManagerService,
-        private _countdownService: CountdownService
-        // private _soundManager: SoundManagerService
+        private _countdownService: CountdownService,
+        private _soundManager: SoundManagerService
     ) { }
 
     public init(): void {
         this._gameTimeManager.resetStartDate();
         this._countdownService.initialize();
+        this._soundManager.playCurrentStartSequenceSound();
     }
 
     public update(racingGame: RacingGame): void {
         if (this._gameTimeManager.getElaspedTime() > ONE_SECOND) {
             this._gameTimeManager.resetStartDate();
             this._countdownService.decreaseOnScreenValue();
+            this._soundManager.playCurrentStartSequenceSound();
         }
         if (this.isStateOver()) {
             racingGame.isCountdownOver = true;
