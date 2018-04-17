63d40ab4fcb16237f9bedeb415d7cdb708a77491
wisev <wsvgny@gmail.com>
2018-04-17 11:18:39 -0400

    [RF-02] State is now an abstract class

--- Cadriciel/client/src/app/racing/game-states/countdownState.ts
+++ Cadriciel/client/src/app/racing/game-states/countdownState.ts
@@ -1,46 +1,36 @@
 import { State } from "./state";
-import { RacingGame } from "../race-game/racingGame";
 import { StateTypes } from "./stateTypes";
-import { GameTimeManagerService } from "../game-time-manager/game-time-manager.service";
-import { CountdownService } from "../countdown/countdown.service";
-import { SoundManagerService } from "../sound-service/sound-manager.service";
 
 const ONE_SECOND: number = 1000;
 const STARTING_TEXT: string = "START";
 
-export class CountdownState implements State {
-
-    public constructor(
-        private _gameTimeManager: GameTimeManagerService,
-        private _countdownService: CountdownService,
-        private _soundManager: SoundManagerService
-    ) { }
+export class CountdownState extends State {
 
     public init(): void {
-        this._gameTimeManager.resetStartDate();
-        this._countdownService.initialize();
-        this._soundManager.playCurrentStartSequenceSound();
+        this._serviceLoader.gameTimeService.resetStartDate();
+        this._serviceLoader.countdownService.initialize();
+        this._serviceLoader.soundService.playCurrentStartSequenceSound();
     }
 
-    public update(racingGame: RacingGame): void {
-        if (this._gameTimeManager.getElaspedTime() > ONE_SECOND) {
-            this._gameTimeManager.resetStartDate();
-            this._countdownService.decreaseOnScreenValue();
-            this._soundManager.playCurrentStartSequenceSound();
+    public update(): void {
+        if (this._serviceLoader.gameTimeService.getElaspedTime() > ONE_SECOND) {
+            this._serviceLoader.gameTimeService.resetStartDate();
+            this._serviceLoader.countdownService.decreaseOnScreenValue();
+            this._serviceLoader.soundService.playCurrentStartSequenceSound();
         }
         if (this.isStateOver()) {
-            racingGame.isCountdownOver = true;
-            racingGame.countdownOnScreenValue = STARTING_TEXT;
-            this.advanceToNextState(racingGame);
+            this._racingGame.isCountdownOver = true;
+            this._racingGame.countdownOnScreenValue = STARTING_TEXT;
+            this.advanceToNextState();
         }
     }
 
     public isStateOver(): boolean {
-        return +this._countdownService.onScreenValue === 0;
+        return +this._serviceLoader.countdownService.onScreenValue === 0;
     }
 
-    public advanceToNextState(racingGame: RacingGame): void {
-        racingGame.setState(StateTypes.Racing);
-        this._gameTimeManager.resetStartDate();
+    public advanceToNextState(): void {
+        this._racingGame.setState(StateTypes.Racing);
+        this._serviceLoader.gameTimeService.resetStartDate();
     }
 }
