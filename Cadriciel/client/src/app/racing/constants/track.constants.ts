4efb10a673df223a91d4ad33eb44b996063991d3
chbinc <chelsy.binet@polymtl.ca>
2018-04-18 18:29:17 -0400

    constants track service

---
+++ Cadriciel/client/src/app/racing/constants/track.constants.ts
@@ -0,0 +1,6 @@
+export const BASE_URL: string = "http://localhost:3000/track";
+export const GET_TRACK_LIST_ERROR: string = "getTrackList";
+export const GET_TRACK_LIST_FROM_ID_ERROR: string = "getTrackFromId";
+export const NEW_TRACK_ERROR: string = "newTrack";
+export const PUT_TRACK_ERROR: string = "putTrack";
+export const DELETE_TRACK_ERROR: string = "getTrackList";
--- Cadriciel/client/src/app/racing/track/track-service/track.service.ts
+++ Cadriciel/client/src/app/racing/track/track-service/track.service.ts
@@ -4,29 +4,31 @@ import { Observable } from "rxjs/Observable";
 import { catchError } from "rxjs/operators";
 import { of } from "rxjs/observable/of";
 import { Track } from "../../../../../../common/racing/track";
+import {
+    BASE_URL,
+    GET_TRACK_LIST_ERROR,
+    GET_TRACK_LIST_FROM_ID_ERROR,
+    NEW_TRACK_ERROR,
+    PUT_TRACK_ERROR,
+    DELETE_TRACK_ERROR
+} from "../../constants/track.constants";
 
 @Injectable()
 export class TrackService {
 
-    private readonly BASE_URL: string = "http://localhost:3000/track";
-    private readonly GET_TRACK_LIST_ERROR: string = "getTrackList";
-    private readonly GET_TRACK_LIST_FROM_ID_ERROR: string = "getTrackFromId";
-    private readonly NEW_TRACK_ERROR: string = "newTrack";
-    private readonly PUT_TRACK_ERROR: string = "putTrack";
-    private readonly DELETE_TRACK_ERROR: string = "getTrackList";
     private readonly httpOptions: { headers: HttpHeaders; } = { headers: new HttpHeaders({}) };
 
     public constructor(private http: HttpClient) { }
 
     public getTrackList(): Observable<Track[]> {
-        return this.http.get<Track[]>(this.BASE_URL).pipe(
-            catchError(this.handleError<Track[]>(this.GET_TRACK_LIST_ERROR))
+        return this.http.get<Track[]>(BASE_URL).pipe(
+            catchError(this.handleError<Track[]>(GET_TRACK_LIST_ERROR))
         );
     }
 
     public getTrackFromId(id: string): Observable<Track> {
-        return this.http.get<Track>(this.BASE_URL + "/" + id).pipe(
-            catchError(this.handleError<Track>(this.GET_TRACK_LIST_FROM_ID_ERROR))
+        return this.http.get<Track>(BASE_URL + "/" + id).pipe(
+            catchError(this.handleError<Track>(GET_TRACK_LIST_FROM_ID_ERROR))
         );
     }
 
@@ -34,20 +36,20 @@ export class TrackService {
         const newTrack: Track = new Track(undefined);
         newTrack.name = trackName;
 
-        return this.http.post<Track>(this.BASE_URL + "/new", newTrack, this.httpOptions).pipe(
-            catchError(this.handleError<Track>(this.NEW_TRACK_ERROR))
+        return this.http.post<Track>(BASE_URL + "/new", newTrack, this.httpOptions).pipe(
+            catchError(this.handleError<Track>(NEW_TRACK_ERROR))
         );
     }
 
     public putTrack(trackId: string, track: Track): Observable<Track> {
-        return this.http.put<Track>(this.BASE_URL + "/put/" + trackId, track, this.httpOptions).pipe(
-            catchError(this.handleError<Track>(this.PUT_TRACK_ERROR))
+        return this.http.put<Track>(BASE_URL + "/put/" + trackId, track, this.httpOptions).pipe(
+            catchError(this.handleError<Track>(PUT_TRACK_ERROR))
         );
     }
 
     public deleteTrack(trackId: string): Observable<Track[]> {
-        return this.http.delete<Track[]>(this.BASE_URL + "/delete/" + trackId, this.httpOptions).pipe(
-            catchError(this.handleError<Track[]>(this.DELETE_TRACK_ERROR))
+        return this.http.delete<Track[]>(BASE_URL + "/delete/" + trackId, this.httpOptions).pipe(
+            catchError(this.handleError<Track[]>(DELETE_TRACK_ERROR))
         );
     }
 
