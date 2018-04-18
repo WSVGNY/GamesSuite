343141814ea3f62ae852ab18216eea1dac263de0
silegc <simon.legault@polymtl.ca>
2018-04-18 16:11:54 -0400

    [REVIEW] Oops forgot some constants in crosswordClient

--- Cadriciel/client/src/app/crossword/crosswordConstants.ts
+++ Cadriciel/client/src/app/crossword/crosswordConstants.ts
@@ -4,6 +4,8 @@ export const VERTICAL: number = 1;
 export const NOT_COLORED_DEFINITION: string = "transparent";
 export const BLACK: string = "black";
 export const WHITE: string = "white";
+export const FIRST_PLAYER_COLOR: string = "steelblue";
+export const SERVER_URL: string = "http://localhost:3000";
 
 export enum State {
     Free = 0,
