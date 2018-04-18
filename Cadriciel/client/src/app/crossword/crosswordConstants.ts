29023a6f58d5d84372e702dd80ae43134eb9c102
silegc <simon.legault@polymtl.ca>
2018-04-18 16:00:45 -0400

    [REVIEW] Crossword client side

---
+++ Cadriciel/client/src/app/crossword/crosswordConstants.ts
@@ -0,0 +1,12 @@
+export const BACKSPACE_KEYCODE: number = 8;
+export const HORIZONTAL: number = 0;
+export const VERTICAL: number = 1;
+export const NOT_COLORED_DEFINITION: string = "transparent";
+export const BLACK: string = "black";
+export const WHITE: string = "white";
+
+export enum State {
+    Free = 0,
+    Selected,
+    Found
+}
