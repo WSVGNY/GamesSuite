95fe6f2a9d7b3d8aea2e673d4df13cf5fd8b7d91
silegc <simon.legault@polymtl.ca>
2018-04-18 16:52:12 -0400

    [REFACTOR] Function for pushTime

--- Cadriciel/client/src/app/racing/game-states/resultsState.ts
+++ Cadriciel/client/src/app/racing/game-states/resultsState.ts
@@ -1,6 +1,7 @@
 import { State } from "./state";
 import { StateTypes } from "./stateTypes";
 import { NUMBER_OF_LAPS } from "../constants/car.constants";
+import { AbstractCar } from "../car/abstractCar";
 
 export class ResultsState extends State {
 
@@ -9,17 +10,21 @@ export class ResultsState extends State {
     public update(): void {
         for (const car of this._racingGame.cars) {
             if (car.raceProgressTracker.lapCount <= NUMBER_OF_LAPS) {
-                this._racingGame.getPlayerByUniqueId(car.uniqueid).pushLapTime(
-                    this._serviceLoader.gameTimeService.simulateRaceTime(
-                        car.raceProgressTracker,
-                        car.currentPosition,
-                        this._racingGame.gameScene.trackMesh.trackPoints.toVectors3
-                    )
-                );
+                this.pushLapTime(car);
                 car.raceProgressTracker.isTimeLogged = true;
             }
         }
         this.advanceToNextState();
+    }
+
+    private pushLapTime(car: AbstractCar): void {
+        this._racingGame.getPlayerByUniqueId(car.uniqueid).pushLapTime(
+            this._serviceLoader.gameTimeService.simulateRaceTime(
+                car.raceProgressTracker,
+                car.currentPosition,
+                this._racingGame.gameScene.trackMesh.trackPoints.toVectors3
+            )
+        );
     }
 
     public isStateOver(): boolean {
