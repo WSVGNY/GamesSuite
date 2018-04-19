95911cc02c1bbe8897c68efe6e9873ec2c604ab8
charlesnollet <chrlsnllt@gmail.com>
2018-04-18 21:19:22 -0400

    Revert "[REVIEW] fixed radius to 30"
    This reverts commit ab9fe13b48c6ea185fdb7d9c5e0b871a5424b8b0.

--- Cadriciel/client/src/app/racing/constants/car.constants.ts
+++ Cadriciel/client/src/app/racing/constants/car.constants.ts
@@ -1,7 +1,7 @@
 export const RPM_FACTOR: number = 1700;
 export const TIRE_ASPHALT_COEFFICIENT: number = 0.72;
 export const MINIMUM_CAR_DISTANCE: number = 3;
-export const TRACKING_SPHERE_RADIUS: number = 30;
+export const TRACKING_SPHERE_RADIUS: number = 25;
 export const NUMBER_OF_LAPS: number = 3;
 export const DEFAULT_WHEELBASE: number = 2.78;
 export const DEFAULT_MASS: number = 1515;
